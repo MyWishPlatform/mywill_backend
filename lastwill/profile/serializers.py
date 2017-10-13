99a643986bf1dce813f0ca7ca7ac727a9c66f0bd
udalov <aleksei.udalov.01@gmail.com>
2017-10-13 17:20:25 +0300

    fix

--- lastwill/profile/serializers.py
+++ lastwill/profile/serializers.py
@@ -7,9 +7,9 @@ class UserRegisterSerializer(RegisterSerializer):
         if request.user.is_anonymous or request.user.password: # anon or normal user
             return super().save(request)
         else: # ghost
-            request.user.username = request.POST['username']
-            request.user.email = request.POST['email']
-            request.user.set_password(request.POST['password1'])
+            request.user.username = request.data['username']
+            request.user.email = request.data['email']
+            request.user.set_password(request.data['password1'])
             request.user.save()
             setup_user_email(request, request.user, [])
             return request.user
