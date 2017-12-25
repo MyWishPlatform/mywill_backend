be666e2e3a76b411cb5ef3ee250685dc7814360e
aleksei.udalov <aleksei.udalov.01@gmail.com>
2017-12-25 18:07:26 +0300

    profile model

---
+++ lastwill/profile/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import User
+from lastwill.consts import MAX_WEI_DIGITS
+
+class Profile(models.Model):
+    user = models.OneToOneField(User)
+    balance = models.DecimalField(max_digits=MAX_WEI_DIGITS, decimal_places=0, default=0)
+    internal_address = models.CharField(max_length=50, null=True, default=None)
+    internal_btc_address = models.CharField(max_length=50, null=True, default=None)
