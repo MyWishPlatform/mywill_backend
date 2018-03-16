b0e43b2ec46f4026871ed9cf0d5c5783be0c9421
aleksei.udalov <aleksei.udalov.01@gmail.com>
2018-03-16 19:44:21 +0300

    emails

---
+++ email_messages.py
@@ -0,0 +1,22 @@
+common_subject = """Your contract is ready"""
+common_text = """Hello,
+
+We are happy to inform you that your contract was successfully created and deployed to Ethereum network.
+{contract_type_name}: https://etherscan.io/address/{address}
+
+Please contact support@mywish.io if you need if you have any questions.
+
+Best wishes,
+MyWish Team."""
+
+ico_subject = """Your contract is ready"""
+ico_text = """Hello,
+
+We are happy to inform you that your contract was successfully created and deployed to Ethereum network.
+Token contract: https://etherscan.io/address/{token_address}
+Crowdsale contract: https://etherscan.io/address/{crowdsale_address}
+
+Please contact support@mywish.io if you have any questions.
+
+Best wishes,
+MyWish Team."""
