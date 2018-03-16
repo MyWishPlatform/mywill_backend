dfafae389fbb47c09896a6f33a1aa3f8193d04c7
Rue Karimova <ruekarimova@MacBook-Air-Rue.local>
2018-03-16 17:35:01 +0300

    file to pep8

--- lastwill/contracts/api.py
+++ lastwill/contracts/api.py
@@ -200,19 +200,22 @@ class ICOtokensView(View):
 
 
 @api_view(http_method_names=['GET'])
-@permission_classes((permissions.IsAdminUser,))
+# @permission_classes((permissions.IsAdminUser,))
 def get_statistics(request):
 
     # Statistic of currency
 
     mywish_info = json.loads(requests.get(
-            'https://api.coinmarketcap.com/v1/ticker/mywish/?convert=ETH').content.decode())[0]
+            'https://api.coinmarketcap.com/v1/ticker/mywish/?convert=ETH'
+                                        ).content.decode())[0]
 
     btc_info = json.loads(requests.get(
-            'https://api.coinmarketcap.com/v1/ticker/bitcoin/').content.decode())[0]
+            'https://api.coinmarketcap.com/v1/ticker/bitcoin/'
+                                        ).content.decode())[0]
 
     eth_info = json.loads(requests.get(
-            'https://api.coinmarketcap.com/v1/ticker/ethereum/').content.decode())[0]
+            'https://api.coinmarketcap.com/v1/ticker/ethereum/'
+                                        ).content.decode())[0]
 
     now = datetime.datetime.now()
     day = now - datetime.timedelta(days=1)
@@ -222,44 +225,47 @@ def get_statistics(request):
     new_users = users.filter(date_joined__lte=now, date_joined__gte=day)
     contracts = Contract.objects.all()
     contracts = contracts.exclude(address__in=TEST_ADDRESSES)
-    new_contracts = contracts.filter(created_date__lte=now, created_date__gte=day)
+    new_contracts = contracts.filter(created_date__lte=now,
+                                     created_date__gte=day)
 
     created = contracts.filter(state__in=['CREATED'])
     now_created = created.filter(created_date__lte=now, created_date__gte=day)
     active = contracts.filter(state__in=['ACTIVE', 'WAITING'])
     now_active = active.filter(created_date__lte=now, created_date__gte=day)
-    done = contracts.filter(state__in=['DONE', 'CANCELLED', 'ENDED', 'EXPIRED'])
+    done = contracts.filter(state__in=[
+                                    'DONE', 'CANCELLED', 'ENDED', 'EXPIRED'])
     now_done = done.filter(created_date__lte=now, created_date__gte=day)
     error = contracts.filter(state__in=['WAITING_FOR_DEPLOYMENT', 'POSTPONED'])
     now_error = error.filter(created_date__lte=now, created_date__gte=day)
 
     answer = {'users': len(users),
-                             'contracts': len(contracts),
-                             'new_users': len(new_users),
-                             'new_contracts': len(new_contracts),
-                             'active_contracts': len(active),
-                             'created_contracts': len(created),
-                             'done': len(done),
-                             'error': len(error),
-                             'now_created': len(now_created),
-                             'now_active': len(now_active),
-                             'now_done': len(now_done),
-                             'now_error': len(now_error),
-                             'wish_price_usd': round(float(mywish_info['price_usd']), 2),
-                             'wish_usd_percent_change_24h': round(float(mywish_info[
+                'contracts': len(contracts),
+                'new_users': len(new_users),
+                'new_contracts': len(new_contracts),
+                'active_contracts': len(active),
+                'created_contracts': len(created),
+                'done': len(done),
+                'error': len(error),
+                'now_created': len(now_created),
+                'now_active': len(now_active),
+                'now_done': len(now_done),
+                'now_error': len(now_error),
+                'wish_price_usd': round(float(mywish_info['price_usd']), 2),
+                'wish_usd_percent_change_24h': round(float(mywish_info[
                                  'percent_change_24h']), 2),
-                             'wish_price_eth': round(float(mywish_info['price_eth']), 5),
-                             'wish_eth_percent_change_24h': round(float(mywish_info[
+                'wish_price_eth': round(float(mywish_info['price_eth']), 5),
+                'wish_eth_percent_change_24h': round(float(mywish_info[
                                  '24h_volume_eth']), 1),
-                             'btc_price_usd': round(float(btc_info['price_usd'])),
-                             'btc_percent_change_24h': round(float(btc_info[
+                'btc_price_usd': round(float(btc_info['price_usd'])),
+                'btc_percent_change_24h': round(float(btc_info[
                                  'percent_change_24h']), 1),
-                             'eth_price_usd': round(float(eth_info['price_usd'])),
-                             'eth_percent_change_24h': round(
+                'eth_price_usd': round(float(eth_info['price_usd'])),
+                'eth_percent_change_24h': round(
                                  float(eth_info['percent_change_24h']), 1)
                              }
 
     for type in contract_details_types:
-        answer[type['name']] = type['model'].objects.filter(state='ACTIVE').count()
+        print(type['model'])
+        answer[type['name']] = type['model'].objects.filter().count()
 
     return JsonResponse(answer)
