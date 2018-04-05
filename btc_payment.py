e1dcad189ac288e6208c41be648561c6aaa68e6c
udalov <aleksei.udalov.01@gmail.com>
2018-04-05 18:57:59 +0300

    btc payment fix

--- btc_payment.py
+++ btc_payment.py
@@ -21,6 +21,8 @@ sleep(10)
 while 1:
     for user in User.objects.exclude(email='', password='', last_name='', first_name=''):
         btc_account = user.btcaccount_set.first()
+        if btc_account is None:
+            continue
         r = requests.post('http://user:password@127.0.0.1:8332/', json={
                 'method': 'getreceivedbyaddress',
                 'jsonrpc': '1.0',
