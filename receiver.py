c9d0106a0159d950faae3c887363b460c1d4d786
aleksei-udalov <aleksei.udalov.01@gmail.com>
2018-05-10 15:18:19 +0200

    lastwill reset fix

--- receiver.py
+++ receiver.py
@@ -152,7 +152,6 @@ class Receiver():
         contract = EthContract.objects.get(id=message['contractId']).contract
         details = contract.get_details()
         details.last_reset = timezone.now()
-        details.last_press_imalive = timezone.now()
         details.save()
         print('notified ok')
 
