0f9dda28233c7f934462a26fc68b1385dc323998
karriva <nina.m.lukina@gmail.com>
2018-05-22 15:14:43 +0300

    add token_holders in to_representation neo contract

--- lastwill/contracts/serializers.py
+++ lastwill/contracts/serializers.py
@@ -500,6 +500,13 @@ class ContractDetailsNeoSerializer(serializers.ModelSerializer):
 
     def to_representation(self, contract_details):
         res = super().to_representation(contract_details)
+        token_holder_serializer = TokenHolderSerializer()
+        res['token_holders'] = [
+            token_holder_serializer.to_representation(th)
+            for th in
+            contract_details.contract.tokenholder_set.order_by(
+                        'id').all()
+            ]
         if not contract_details:
            print('*'*50, contract_details.id, flush=True)
         # res['eth_contract'] = EthContractSerializer().to_representation(contract_details.eth_contract)
