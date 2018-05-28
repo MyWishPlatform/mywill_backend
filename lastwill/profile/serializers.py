a3a6971937568fca89306ab0448800b7dc2d4aaa
karriva <nina.m.lukina@gmail.com>
2018-05-28 14:23:10 +0300

    delete auto generating btc address

--- lastwill/profile/serializers.py
+++ lastwill/profile/serializers.py
@@ -23,11 +23,7 @@ def init_profile(user, is_social=False):
         user=user, internal_address=internal_address, is_social=is_social
     ).save()
     with transaction.atomic():
-        # btc_account = BTCAccount.objects.filter(user__isnull=True).first()
-        # root_public_key = ''
-        btc_string = ROOT_PUBLIC_KEY + str(user.id)
-        address = bitcoin.privkey_to_address(btc_string)
-        btc_account = BTCAccount(address=address)
+        btc_account = BTCAccount.objects.filter(user__isnull=True).first()
         btc_account.user = user
         btc_account.save()
 
