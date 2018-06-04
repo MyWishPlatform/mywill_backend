2a9463f4d83359fb2c1784807910fb5d7254d21a
karriva <nina.m.lukina@gmail.com>
2018-06-04 17:43:32 +0300

    bug fix

--- lastwill/contracts/models.py
+++ lastwill/contracts/models.py
@@ -1409,8 +1409,8 @@ class ContractDetailsNeo(CommonDetails):
         for ind, th in enumerate(token_holders):
             preproc_params["constants"]["D_PREMINT_ADDRESS_" + str(ind)] = str(th.address)
             preproc_params["constants"]["D_PREMINT_AMOUNT_" + str(ind)] = str(
-                [int(x) for x in th.amount.to_bytes(
-                    math.ceil(math.log(th.amount, 256)
+                [int(x) for x in int(th.amount).to_bytes(
+                    math.ceil(math.log(int(th.amount), 256)
                               ), 'little')]
             )
             preproc_params["constants"]["D_PREMINT_FREEZE_" + str(ind)] = str(th.freeze_date) if th.freeze_date else 0
@@ -1622,8 +1622,8 @@ class ContractDetailsNeoICO(CommonDetails):
         for ind, th in enumerate(token_holders):
             preproc_params["constants"]["D_PREMINT_ADDRESS_" + str(ind)] = str(th.address)
             preproc_params["constants"]["D_PREMINT_AMOUNT_" + str(ind)] = str(
-                [int(x) for x in th.amount.to_bytes(
-                    math.ceil(math.log(th.amount, 256)
+                [int(x) for x in int(th.amount).to_bytes(
+                    math.ceil(math.log(int(th.amount), 256)
                               ), 'little')]
             )
             preproc_params["constants"]["D_PREMINT_FREEZE_" + str(ind)] = str(th.freeze_date) if th.freeze_date else 0
