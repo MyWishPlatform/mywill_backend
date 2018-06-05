18717b3fa7fed2a126d1ec8c53dd024e72af714c
karriva <nina.m.lukina@gmail.com>
2018-06-05 18:18:20 +0300

    add logging in receiver

--- receiver.py
+++ receiver.py
@@ -44,84 +44,108 @@ class Receiver():
 
     def deployed(self, message):
         print('deployed message received', flush=True)
-        test_logger.info()
+        test_logger.info('RECEIVER: deployed message')
         contract = EthContract.objects.get(id=message['contractId']).contract
         contract.get_details().msg_deployed(message)
         print('deployed ok!', flush=True)
+        test_logger.info('RECEIVER: deployed ok')
 
     def killed(self, message):
         print('killed message', flush=True)
+        test_logger.info('RECEIVER: killed message')
         contract = EthContract.objects.get(id=message['contractId']).contract
         contract.state = 'KILLED'
         contract.save()
         network = contract.network
         DeployAddress.objects.filter(network=network, locked_by=contract.id).update(locked_by=None)
         print('killed ok', flush=True)
+        test_logger.info('RECEIVER: killed ok')
 
     def checked(self, message):
         print('checked message', flush=True)
+        test_logger.info('RECEIVER: checked message')
         contract = EthContract.objects.get(id=message['contractId']).contract
         contract.get_details().checked(message)
         print('checked ok', flush=True)
+        test_logger.info('RECEIVER: checked ok')
 
     def repeat_check(self, message):
         print('repeat check message', flush=True)
+        test_logger.info('RECEIVER: repeat check message')
         contract = EthContract.objects.get(id=message['contractId']).contract
         contract.get_details().check_contract()
         print('repeat check ok', flush=True)
+        test_logger.info('RECEIVER: repeat check ok')
 
     def check_contract(self, message):
         print('check contract message', flush=True)
+        test_logger.info('RECEIVER: check contract message')
         contract = Contract.objects.get(id=message['contractId'])
         contract.get_details().check_contract()
         print('check contract ok', flush=True)
+        test_logger.info('RECEIVER: check contract ok')
 
     def triggered(self, message):
         print('triggered message', flush=True)
+        test_logger.info('RECEIVER: triggered message')
         contract = EthContract.objects.get(id=message['contractId']).contract
         contract.get_details().triggered(message)
         print('triggered ok', flush=True)
+        test_logger.info('RECEIVER: triggered ok')
 
     def launch(self, message):
         print('launch message', flush=True)
+        test_logger.info('RECEIVER: launch message')
         try:
             contract_details = Contract.objects.get(id=message['contractId']).get_details()
             contract_details.deploy()
         except ObjectDoesNotExist:
             # only when contract removed manually
             print('no contract, ignoging')
+            test_logger.error('RECEIVER: no contract')
             return
         contract_details.refresh_from_db()
         print('launch ok')
+        test_logger.info('RECEIVER: launch ok')
 
     def ownershipTransferred(self, message):
         print('ownershipTransferred message')
+        test_logger.info('RECEIVER: ownershipTransferred message')
         contract = EthContract.objects.get(id=message['crowdsaleId']).contract
         contract.get_details().ownershipTransferred(message)
         print('ownershipTransferred ok')
+        test_logger.info('RECEIVER: ownershipTransferred ok')
 
     def initialized(self, message):
         print('initialized message')
+        test_logger.info('RECEIVER: initialized message')
         contract = EthContract.objects.get(id=message['contractId']).contract
         contract.get_details().initialized(message)
         print('initialized ok')
+        test_logger.info('RECEIVER: in initialized ok')
 
     def finish(self, message):
         print('finish message')
+        test_logger.info('RECEIVER: finish message')
         contract = EthContract.objects.get(id=message['contractId']).contract
         contract.get_details().finalized(message)
         print('finish ok')
+        test_logger.info('RECEIVER: finish ok')
 
     def finalized(self, message):
         print('finalized message')
+        test_logger.info('RECEIVER: finalized message')
         contract = EthContract.objects.get(id=message['contractId']).contract
         contract.get_details().finalized(message)
         print('finalized ok')
+        test_logger.info('RECEIVER: finalized ok')
 
     def transactionCompleted(self, message):
         print('transactionCompleted')
+        test_logger.info('RECEIVER: transactionCompleted')
         if message['transactionStatus']:
             print('success, ignoring')
+            test_logger.info('RECEIVER:  success')
             return
         try:
             contract = EthContract.objects.get(tx_hash=message['transactionHash']).contract
@@ -129,49 +153,66 @@ class Receiver():
         except Exception as e:
             print(e)
             print('not found, returning')
+            test_logger.error('RECEIVER: not found')
             return
         print('transactionCompleted ok')
+        test_logger.info('RECEIVER: transactionCOmpleted ok')
 
     def cancel(self, message):
         print('cancel message')
+        test_logger.info('RECEIVER: cancel message')
         contract = Contract.objects.get(id=message['contractId'])
         contract.get_details().cancel(message)
         print('cancel ok')
+        test_logger.info('RECEIVER: cancel ok')
 
     def confirm_alive(self, message):
         print('confirm_alive message')
+        test_logger.info('RECEIVER: confirm alive message')
         contract = Contract.objects.get(id=message['contractId'])
         contract.get_details().i_am_alive(message)
         print('confirm_alive ok')
+        test_logger.info('RECEIVER: confirm alive ok')
 
     def contractPayment(self, message):
         print('contract Payment message')
+        test_logger.info('RECEIVER: contract payment message')
         contract = Contract.objects.get(id=message['contractId'])
         contract.get_details().contractPayment(message)
         print('contract Payment ok')
+        test_logger.info('RECEIVER: contract payment ok')
 
     def notified(self, message):
         print('notified message')
+        test_logger.info('RECEIVER: notified message')
         contract = EthContract.objects.get(id=message['contractId']).contract
         details = contract.get_details()
         details.last_reset = timezone.now()
         details.save()
         print('notified ok')
+        test_logger.info('RECEIVER: notified ok')
 
     def fundsAdded(self, message):
         print('funds Added message')
+        test_logger.info('RECEIVER: funds added message')
         contract = EthContract.objects.get(id=message['contractId']).contract
         contract.get_details().fundsAdded(message)
         print('funds Added ok')
+        test_logger.info('RECEIVER: funds added ok')
 
     def make_payment(self, message):
         print('make payment message')
+        test_logger.info('RECEIVER: make payment message')
         contract = Contract.objects.get(id=message['contractId'])
         contract.get_details().make_payment(message)
         print('make payment ok')
+        test_logger.info('RECEIVER: make payment ok')
 
     def callback(self, ch, method, properties, body):
-
+        test_logger.info('RECEIVER: callback params')
+        test_logger.info(str(body))
+        test_logger.info(str(properties))
+        test_logger.info(str(method))
         print('received', body, properties, method, flush=True)
         try:
             message = json.loads(body.decode())
@@ -181,6 +222,7 @@ class Receiver():
             ch.basic_ack(delivery_tag=method.delivery_tag)
         except NeedRequeue:
             print('requeueing message', flush=True)
+            test_logger.error('RECEIVER: requeueing message')
             ch.basic_nack(delivery_tag=method.delivery_tag, requeue=True)
         except Exception as e:
             print('\n'.join(traceback.format_exception(*sys.exc_info())),
@@ -190,6 +232,7 @@ class Receiver():
 
     def unknown_handler(self, message):
         print('unknown message', message, flush=True)
+        test_logger.error('RECEIVER: unknown message')
 
 
 def methods(cls):
@@ -223,5 +266,6 @@ for net in nets:
     )
     channel.basic_consume(rec.callback, queue=NETWORKS[net]['queue'])
 
+test_logger.info('RECEIVER: receiver started')
 print('receiver started', flush=True)
 channel.start_consuming()
