d6638ba5f62f0fb839f2bd41b38a609dd804ffed
karriva <nina.m.lukina@gmail.com>
2018-06-14 19:46:38 +0300

    bug fix

--- lastwill/contracts/models.py
+++ lastwill/contracts/models.py
@@ -1714,14 +1714,14 @@ class ContractDetailsNeoICO(CommonDetails):
         with open(path.join(
                 dest,
                 'Crowdsale.Contract/bin/Release/netcoreapp2.0/publish/Crowdsale.Contract.abi.json'
-        )) as f:
+        ), 'rb') as f:
             token_json = json.loads(f.read().decode('utf-8-sig'))
         with open(path.join(
                 dest,
                 'Crowdsale.Contract/bin/Release/netcoreapp2.0/publish/Crowdsale.Contract.avm'
         ), mode='rb') as f:
             bytecode = f.read()
-        with open(path.join(dest, 'Crowdsale.Contract/Crowdsale.cs')) as f:
+        with open(path.join(dest, 'Crowdsale.Contract/Crowdsale.cs'), 'rb') as f:
             source_code = f.read().decode('utf-8-sig')
         neo_contract_crowdsale = NeoContract()
         neo_contract_crowdsale.abi = token_json
