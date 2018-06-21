d4dec13649173004d310b8e47f08bf44c7ec1415
karriva <nina.m.lukina@gmail.com>
2018-06-21 17:14:48 +0300

    bug fix

--- lastwill/contracts/api.py
+++ lastwill/contracts/api.py
@@ -11,7 +11,8 @@ from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
-from rest_framework.exceptions import PermissionDenied, BasePermission, SAFE_METHODS
+from rest_framework.permissions import BasePermission
+from rest_framework.exceptions import PermissionDenied, SAFE_METHODS
 from rest_framework.decorators import api_view
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import ValidationError
