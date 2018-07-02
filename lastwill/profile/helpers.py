0d42d39ad5792580b4887e91330408fc739a209c
aleksei-udalov <aleksei.udalov.01@gmail.com>
2018-07-02 17:51:58 +0200

    totp reuse fix

--- lastwill/profile/helpers.py
+++ lastwill/profile/helpers.py
@@ -7,5 +7,5 @@ def valid_totp(user, totp):
     if user.profile.last_used_totp is not None and user.profile.last_used_totp == totp:
         return False
     user.profile.last_used_totp = totp
-    user.profile.save(update_fields=['totp'])
+    user.profile.save(update_fields=['last_used_totp'])
     return True
