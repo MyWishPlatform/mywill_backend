d4effe0f829ff38fd91790f02fb1fb497e06db55
karriva <nina.m.lukina@gmail.com>
2018-07-12 14:29:04 +0300

    bug fix

--- currency_proxi/manage.py
+++ currency_proxi/manage.py
@@ -3,12 +3,12 @@ import os
 from flask.ext.script import Manager
 from flask.ext.migrate import Migrate, MigrateCommand
 
-from app import app, db
+from app import app
 #from config import Config
 
 #app.config(Config)
 
-migrate = Migrate(app, db)
+# migrate = Migrate(app, db)
 manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
