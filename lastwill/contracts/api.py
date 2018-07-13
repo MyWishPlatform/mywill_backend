1ae265717e4588f52ef2cc33fb399acb5c70dac5
karriva <nina.m.lukina@gmail.com>
2018-07-13 19:03:21 +0300

    add view with invests balance

--- lastwill/contracts/api.py
+++ lastwill/contracts/api.py
@@ -23,7 +23,7 @@ from lastwill.parint import *
 from lastwill.profile.models import Profile
 from lastwill.promo.models import Promo, User2Promo
 from lastwill.promo.api import check_and_get_discount
-from lastwill.contracts.models import Contract, WhitelistAddress, AirdropAddress, EthContract, send_in_queue, ContractDetailsInvestmentPool
+from lastwill.contracts.models import Contract, WhitelistAddress, AirdropAddress, EthContract, send_in_queue, ContractDetailsInvestmentPool, InvestAddress
 from lastwill.deploy.models import Network
 from lastwill.payments.api import create_payment
 import lastwill.check as check
@@ -486,3 +486,13 @@ def get_contract_for_link(request):
     )
     contract = details.contract
     return JsonResponse(ContractSerializer().to_representation(contract))
+
+@api_view(http_method_names=['GET'])
+def get_invest_balance_day(request):
+    contract = Contract.objects.get(id=request.data.get('id'))
+    date = datetime.datetime.now().date()
+    invests = InvestAddress.objects.filter(contract=contract, created_date__lte=date)
+    balance = 0
+    for inv in invests:
+        balance = balance + inv.amount
+    return JsonResponse({'last_balance': balance})
