83a901acbe07470a158c03976920fd11cb4b0aa3
karriva <nina.m.lukina@gmail.com>
2018-07-23 17:04:49 +0300

    add details for eos token

---
+++ lastwill/contracts/submodels/eos.py
@@ -0,0 +1,46 @@
+from django.db import models
+from django.utils import timezone
+from rest_framework.exceptions import ValidationError
+
+from lastwill.contracts.submodels.common import *
+
+
+class ContractDetailsEOSToken(CommonDetails):
+    token_name = models.CharField(max_length=512)
+    token_short_name = models.CharField(max_length=64)
+    admin_address = models.CharField(max_length=50)
+    decimals = models.IntegerField()
+    token_type = models.CharField(max_length=32, default='ERC20')
+    type_payments = models.CharField(max_length=50)
+    eth_contract_token = models.ForeignKey(
+        EthContract,
+        null=True,
+        default=None,
+        related_name='token_details_token',
+        on_delete=models.SET_NULL
+    )
+    future_minting = models.BooleanField(default=False)
+    temp_directory = models.CharField(max_length=36)
+
+    def predeploy_validate(self):
+        now = timezone.now()
+        token_holders = self.contract.tokenholder_set.all()
+        for th in token_holders:
+            if th.freeze_date:
+                if th.freeze_date < now.timestamp() + 600:
+                    raise ValidationError({'result': 1}, code=400)
+
+    @classmethod
+    def min_cost(cls):
+        network = Network.objects.get(name='EOS_MAINNET')
+        cost = cls.calc_cost({}, network)
+        return cost
+
+    @staticmethod
+    def calc_cost(kwargs, network):
+        if NETWORKS[network.name]['is_free']:
+            return 0
+        return int(0.99 * 10 ** 18)
+
+    def get_arguments(self, eth_contract_attr_name):
+        return []
