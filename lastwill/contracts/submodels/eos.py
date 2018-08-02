cf4b271a44d855f3c2c4e7d81b977e122d0d8669
karriva <nina.m.lukina@gmail.com>
2018-08-02 18:52:55 +0300

    bug fix

--- lastwill/contracts/submodels/eos.py
+++ lastwill/contracts/submodels/eos.py
@@ -207,8 +207,8 @@ class ContractDetailsEOSAccount(CommonDetails):
             'cleos', '-u', EOS_URL, 'system', 'newaccount',
             EOS_ACCOUNT_NAME, self.account_name, self.owner_public_key,
             self.active_public_key, '--stake-net', '"%s"' % (str(self.stake_net_value) + ' EOS'),
-            ' --stake-cpu ', '"%s"' % (str(self.stake_cpu_value) + ' EOS'),
-            '--buy-ram-kbytes ' + str(self.buy_ram_kbytes)
+            '--stake-cpu', '"%s"' % (str(self.stake_cpu_value) + ' EOS'),
+            '--buy-ram-kbytes' + str(self.buy_ram_kbytes)
         ]
         print('command = ', command)
         result = Popen(command, stdin=PIPE, stdout=PIPE, stderr=PIPE).communicate()
