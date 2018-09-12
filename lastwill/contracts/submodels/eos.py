9702c97dc7e929ef0308cdf9085bdecec13e42bf
karriva <nina.m.lukina@gmail.com>
2018-09-12 19:11:37 +0300

    change min cost

--- lastwill/contracts/submodels/eos.py
+++ lastwill/contracts/submodels/eos.py
@@ -183,7 +183,7 @@ class ContractDetailsEOSAccount(CommonDetails):
             if result:
                 ram = json.loads(stdout.decode())['rows'][0]
                 print(result)
-                ram_price = float(ram['quote']['balance']) / float(ram['base']['balance']) * 1024
+                ram_price = float(ram['quote']['balance'].split()) / float(ram['base']['balance'].split()) * 1024
                 break
         else:
             print('stderr', stderr, flush=True)
