e248b4dcc6a178fe7c6a64eacdd9a60f7cb0dfcb
karriva <nina.m.lukina@gmail.com>
2018-09-24 19:05:12 +0300

    add function for create promo

--- lastwill/promo/api.py
+++ lastwill/promo/api.py
@@ -30,3 +30,28 @@ def get_discount(request):
     promo_str = request.query_params['promo']
     discount = check_and_get_discount(promo_str, contract_type, user)
     return Response({'discount': discount})
+
+
+def create_promocode(
+        promo_str, contract_types, discount, start=None,
+        stop=None, use_count=0, use_count_max=None
+):
+    promo = Promo.objects.filter(promo_str=promo_str.upper()).first()
+    if promo is not None:
+        print('this promocode already exists')
+        return
+    else:
+        if start is None and stop is None:
+            start = datetime.datetime.now().date()
+            stop = datetime.datetime(start.year+1, start.month, start.day).date()
+        promo = Promo(
+            promo_str=promo_str, start=start, stop=stop,
+            use_count=use_count, use_count_max = use_count_max
+        )
+        promo.save()
+        for ct in contract_types:
+            p2c = Promo2ContractType(
+                promo=promo, discount=discount, contract_type=ct
+            )
+            p2c.save()
+        return promo
