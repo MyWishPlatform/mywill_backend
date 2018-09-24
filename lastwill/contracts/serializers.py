550068c781fabd4d8cdbae3446d9aa1fdf161ff0
karriva <nina.m.lukina@gmail.com>
2018-09-24 15:06:17 +0300

    bug fix

--- lastwill/contracts/serializers.py
+++ lastwill/contracts/serializers.py
@@ -1031,7 +1031,7 @@ class ContractDetailsEOSAirdropSerializer(serializers.ModelSerializer):
     def to_representation(self, contract_details):
         res = super().to_representation(contract_details)
         res['eos_contract'] = EOSContractSerializer().to_representation(contract_details.eos_contract)
-        res['added_count'] = contract_details.contract.airdropaddress_set.filter(state='added', active=True).count()
+        res['added_count'] = contract_details.contract.eosairdropaddress_set.filter(state='added', active=True).count()
         res['processing_count'] = contract_details.contract.eosairdropaddress_set.filter(state='processing', active=True).count()
         res['sent_count'] = contract_details.contract.eosairdropaddress_set.filter(state='sent', active=True).count()
         return res
