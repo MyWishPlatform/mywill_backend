eb60f39972bb018f6466278a5995c9c2330c2f9c
karriva <nina.m.lukina@gmail.com>
2018-11-07 13:41:08 +0300

    bug fix - price eos airdrop

--- lastwill/contracts/submodels/eos.py
+++ lastwill/contracts/submodels/eos.py
@@ -576,7 +576,7 @@ class ContractDetailsEOSAirdrop(CommonDetails):
     def calc_cost(kwargs, network):
         if NETWORKS[network.name]['is_free']:
             return 0
-        eos_cost = ContractDetailsEOSAirdrop.calc_cost_eos(kwargs, network)
+        eos_cost = ContractDetailsEOSAirdrop.calc_cost_eos(kwargs, network) /10**4
         cost = eos_cost * convert('EOS', 'ETH')['ETH']
         return round(cost, 2) * 10 ** 18
 
