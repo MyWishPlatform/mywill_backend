c2833b7552b4f2d0f2e04fb000f58a8a27021298
karriva <nina.m.lukina@gmail.com>
2018-11-14 14:04:23 +0300

    edit methods create positive/negativ payments

--- lastwill/payments/api.py
+++ lastwill/payments/api.py
@@ -15,55 +15,38 @@ def create_payment(uid, tx, currency, amount, site_id):
         return
     print('create payment')
     user = User.objects.get(id=uid)
-    if currency == 'EOSISH':
-        value = amount
-    elif currency == 'EOS':
-        eosish_exchange_rate = float(
-            requests.get('https://api.chaince.com/tickers/eosisheos/',
-                         headers={'accept-version': 'v1'}).json()['price']
-        )
-        value = amount / eosish_exchange_rate
-    else:
+    if SubSite.objects.get(id=site_id).site_name == 'wish':
         value = amount if currency == 'WISH' else to_wish(
             currency, amount
         )
-    if amount < 0.0:
-        negative_payment(user, currency, -value, site_id)
     else:
-        positive_payment(user, currency, value, site_id)
+        value = amount * convert(currency, 'EOSISH')
+
+    if amount < 0.0:
+        negative_payment(user, -value, site_id)
+    else:
+        positive_payment(user, value, site_id)
 
     payment = InternalPayment(
         user_id=uid,
         delta=value,
         tx_hash=tx,
         original_currency=currency,
-        original_delta=str(amount)
+        original_delta=str(amount),
+        site__id=site_id
     )
     payment.save()
     print('payment created')
 
 
-def positive_payment(user, currency, value, site_id):
-    if currency in ['EOS', 'EOSISH']:
-        Profile.objects.select_for_update().filter(
-            id=user.profile.id).update(
-            eos_balance=F('eos_balance') + value)
-    else:
-        Profile.objects.select_for_update().filter(
-            id=user.profile.id).update(
+def positive_payment(user, value, site_id):
+    UserSiteBalance.select_for_update().filter(
+        user=user, subsite__id=site_id).update(
             balance=F('balance') + value)
 
 
-def negative_payment(user, currency, value, site_id):
-    if currency not in ['EOS', 'EOSISH']:
-
-        if not Profile.objects.select_for_update().filter(
-                user=user, balance__gte=value
-        ).update(balance=F('balance') - value):
-            raise Exception('no money')
-    else:
-        eos_cost = value
-        if not Profile.objects.select_for_update().filter(
-                user=user, eos_balance__gte=eos_cost
-        ).update(eos_balance=F('eos_balance') - eos_cost):
-            raise Exception('no money')
+def negative_payment(user, value, site_id):
+    if not UserSiteBalance.objects.select_for_update().filter(
+            user=user, subsite__id=site_id, balance__gte=value
+    ).update(eos_balance=F('balance') - value):
+        raise Exception('no money')
