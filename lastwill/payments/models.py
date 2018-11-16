746fb98d06c1be5bcc847c167ff62cf5a72b0c5d
karriva <nina.m.lukina@gmail.com>
2018-11-16 14:26:34 +0300

    datetime im internal payments to auto_now_add

--- lastwill/payments/models.py
+++ lastwill/payments/models.py
@@ -8,7 +8,7 @@ class InternalPayment(models.Model):
     user = models.ForeignKey(User)
     delta = models.DecimalField(max_digits=MAX_WEI_DIGITS, decimal_places=0)
     tx_hash = models.CharField(max_length=66, null=True, default='')
-    datetime = models.DateTimeField(auto_now=True)
+    datetime = models.DateTimeField(auto_now_add=True)
     original_currency = models.CharField(max_length=66, null=True, default='')
     original_delta = models.CharField(max_length=66, null=True, default='')
     site = models.ForeignKey(SubSite)
