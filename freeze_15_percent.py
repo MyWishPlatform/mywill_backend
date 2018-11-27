db447143b27a6082a3ba77a4162e4cad5c667609
karriva <nina.m.lukina@gmail.com>
2018-11-27 15:31:18 +0300

    add eth freeze tokens

--- freeze_15_percent.py
+++ freeze_15_percent.py
@@ -1,33 +1,272 @@
 import time
 import os
+import binascii
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lastwill.settings')
 import django
 django.setup()
 
 from django.utils import timezone
 from lastwill.payments.models import *
-from lastwill.settings import FREEZE_THRESHOLD_EOSISH, FREEZE_THRESHOLD_WISH, NETWORKS
+from lastwill.settings import FREEZE_THRESHOLD_EOSISH, FREEZE_THRESHOLD_WISH, NETWORKS, MYWISH_ADDRESS, EOSISH_ACTIVE_KEY
+from lastwill.settings import COLD_EOSISH_ADDRESS, COLD_WISH_ADDRESS,UPDATE_EOSISH_ADDRESS, UPDATE_WISH_ADDRESS
 from lastwill.contracts.models import Contract, implement_cleos_command, unlock_eos_account
+from lastwill.contracts.submodels.common import *
 
 
 def freeze_wish():
-    address_to = '0x2246Bf20f536cbf9ab9a7bC8B3518276Ae40b2F6'
-    pass
+    abi = [
+  {
+    "constant": True,
+    "inputs": [],
+    "name": "name",
+    "outputs": [
+      {
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "payable": False,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": False,
+    "inputs": [
+      {
+        "name": "_spender",
+        "type": "address"
+      },
+      {
+        "name": "_value",
+        "type": "uint256"
+      }
+    ],
+    "name": "approve",
+    "outputs": [
+      {
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "payable": False,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": True,
+    "inputs": [],
+    "name": "totalSupply",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "payable": False,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": False,
+    "inputs": [
+      {
+        "name": "_from",
+        "type": "address"
+      },
+      {
+        "name": "_to",
+        "type": "address"
+      },
+      {
+        "name": "_value",
+        "type": "uint256"
+      }
+    ],
+    "name": "transferFrom",
+    "outputs": [
+      {
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "payable": False,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": True,
+    "inputs": [],
+    "name": "decimals",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint8"
+      }
+    ],
+    "payable": False,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": True,
+    "inputs": [
+      {
+        "name": "_owner",
+        "type": "address"
+      }
+    ],
+    "name": "balanceOf",
+    "outputs": [
+      {
+        "name": "balance",
+        "type": "uint256"
+      }
+    ],
+    "payable": False,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": True,
+    "inputs": [],
+    "name": "symbol",
+    "outputs": [
+      {
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "payable": False,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": False,
+    "inputs": [
+      {
+        "name": "_to",
+        "type": "address"
+      },
+      {
+        "name": "_value",
+        "type": "uint256"
+      }
+    ],
+    "name": "transfer",
+    "outputs": [
+      {
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "payable": False,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": True,
+    "inputs": [
+      {
+        "name": "_owner",
+        "type": "address"
+      },
+      {
+        "name": "_spender",
+        "type": "address"
+      }
+    ],
+    "name": "allowance",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "payable": False,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "payable": True,
+    "stateMutability": "payable",
+    "type": "fallback"
+  },
+  {
+    "anonymous": False,
+    "inputs": [
+      {
+        "indexed": True,
+        "name": "owner",
+        "type": "address"
+      },
+      {
+        "indexed": True,
+        "name": "spender",
+        "type": "address"
+      },
+      {
+        "indexed": False,
+        "name": "value",
+        "type": "uint256"
+      }
+    ],
+    "name": "Approval",
+    "type": "event"
+  },
+  {
+    "anonymous": False,
+    "inputs": [
+      {
+        "indexed": True,
+        "name": "from",
+        "type": "address"
+      },
+      {
+        "indexed": True,
+        "name": "to",
+        "type": "address"
+      },
+      {
+        "indexed": False,
+        "name": "value",
+        "type": "uint256"
+      }
+    ],
+    "name": "Transfer",
+    "type": "event"
+  }
+]
+    tr = abi.ContractTranslator(abi)
+    par_int = ParInt('ETHEREUM_MAINNET')
+    address = NETWORKS['ETHEREUM_MAINNET']['address']
+    nonce = int(par_int.eth_getTransactionCount(address, "pending"), 16)
+    signed_data = sign_transaction(
+      UPDATE_WISH_ADDRESS, nonce,
+      100000,
+      'ETHEREUM_MAINNET',
+      dest=MYWISH_ADDRESS,
+      contract_data=binascii.hexlify(
+        tr.encode_function_call('transfer', [COLD_WISH_ADDRESS, FREEZE_THRESHOLD_WISH])
+      ).decode()
+    )
+    tx_hash = par_int.eth_sendRawTransaction(
+      '0x' + signed_data
+    )
+    print('tx_hash=', tx_hash, flush=True)
 
 
 def freeze_eosish():
     # wallet_name = NETWORKS['EOS_MAINNET']['wallet']
     # password = NETWORKS['EOS_MAINNET']['eos_password']
     # unlock_eos_account(wallet_name, password)
-    public_key = ''
     command_list = [
         'cleos', 'push', 'action', 'eosio.token', 'transfer',
         '[ "{address_from}", "{address_to}", "{amount} EOSISH", "m" ]'.format(
-            address_from='',
-            address_to='eosishfreeze',
+            address_from=UPDATE_EOSISH_ADDRESS,
+            address_to=COLD_EOSISH_ADDRESS,
             amount=FREEZE_THRESHOLD_EOSISH
         ),
-        '-p', public_key
+        '-p', EOSISH_ACTIVE_KEY
     ]
     implement_cleos_command(command_list)
 
