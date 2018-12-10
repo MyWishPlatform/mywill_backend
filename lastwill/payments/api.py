9d1f89d6e251c8d2a38a374baa77285ece690cc0
karriva <nina.m.lukina@gmail.com>
2018-12-10 13:39:04 +0300

    hot fix

--- lastwill/payments/api.py
+++ lastwill/payments/api.py
@@ -24,9 +24,11 @@ def create_payment(uid, tx, currency, amount, site_id):
             amount = amount / 10 ** 18
         if currency in ['BTC']:
             amount = amount / 10 ** 8
-        # if currency == 'EOS':
-        #     amount = amount / 10 ** 4
-        value = amount if currency == 'EOSISH' else amount * convert(currency, 'EOSISH')['EOSISH']
+        value = amount if currency == 'EOSISH' else amount * convert(currency, 'EOSISH')['EOSISH'] * 10 ** 4
+        if currency in ['ETH']:
+            amount = amount * 10 ** 18
+        if currency in ['BTC']:
+            amount = amount * 10 ** 8
 
     if amount < 0.0:
         negative_payment(user, -value, site_id)
