7114f183687e7bb98a9f1f79daa18a0909b6c279
karriva <nina.m.lukina@gmail.com>
2018-12-10 16:05:17 +0300

    ad check authio_email

--- lastwill/contracts/serializers.py
+++ lastwill/contracts/serializers.py
@@ -551,9 +551,9 @@ class ContractDetailsTokenSerializer(serializers.ModelSerializer):
             if th['freeze_date'] is not None and th['freeze_date'] < now:
                 test_logger.error('Error freeze date in token serializer')
                 raise ValidationError({'result': 2}, code=400)
-        # if details['authio']:
-        #     if not details['authio_email']:
-        #         raise ValidationError
+        if details['authio']:
+            if not details['authio_email']:
+                raise ValidationError
 
     def to_representation(self, contract_details):
         res = super().to_representation(contract_details)
