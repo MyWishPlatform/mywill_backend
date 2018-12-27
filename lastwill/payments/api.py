f8dbe489b9507e54a281ce38de63f2e77245fc72
karriva <nina.m.lukina@gmail.com>
2018-12-27 18:32:15 +0300

    bug fix

--- lastwill/payments/api.py
+++ lastwill/payments/api.py
@@ -29,8 +29,7 @@ def create_payment(uid, tx, currency, amount, site_id):
         if currency in ['EOS']:
             amount = amount / 10 ** 4
         value = amount if currency == 'EOSISH' else amount * convert(currency, 'EOSISH')['EOSISH']
-        if value > 0.0:
-            value = value * 10 ** 4
+        value = value * 10 ** 4
         if currency in ['ETH']:
             amount = amount * 10 ** 18
         if currency in ['BTC']:
