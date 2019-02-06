d5e144365ec325c239a92081b2414e280243004f
karriva <nina.m.lukina@gmail.com>
2019-02-06 14:20:14 +0300

    rename id -> contract_id in answer eos api

--- lastwill/contracts/api_eos.py
+++ lastwill/contracts/api_eos.py
@@ -172,7 +172,7 @@ def create_eos_account(request):
     answer = {
         'state': contract.state,
         'address': contract_details.account_name,
-        'id': contract.id,
+        'contract_id': contract.id,
         'created_date': contract.created_date,
         'network': contract.network.name,
         'network_id': contract.network.id,
@@ -247,7 +247,7 @@ def show_eos_account(request):
     answer = {
         'state': contract.state,
         'address': contract_details.account_name,
-        'id': contract.id,
+        'contract_id': contract.id,
         'created_date': contract.created_date,
         'network': contract.network.name,
         'network_id': contract.network.id,
@@ -310,7 +310,7 @@ def edit_eos_account(request):
     answer = {
         'state': contract.state,
         'address': contract_details.account_name,
-        'id': contract.id,
+        'contract_id': contract.id,
         'created_date': contract.created_date,
         'network': contract.network.name,
         'network_id': contract.network.id,
@@ -482,7 +482,7 @@ def get_eos_contracts(request):
     answer = {'contracts': []}
     for c in contracts:
         contract_info = {
-            'id': c.id,
+            'contract_id': c.id,
             'state': c.state,
             'created_date': c.created_date,
             'network': c.network.name,
