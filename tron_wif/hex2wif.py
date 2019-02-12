10c56f480f0d340419a20e7ee9f54cc8d11f2d4c
Maksim Strenk <ephdtrg@gmail.com>
2019-02-12 14:31:43 +0300

    fix path for hex2wif script

--- tron_wif/hex2wif.py
+++ tron_wif/hex2wif.py
@@ -1,10 +1,15 @@
 import sys
+import os
 
 from Naked.toolshed.shell import execute_js, muterun_js
 
 
 def hex2tronwif(hexstr):
-    response = muterun_js('hextob58.js', hexstr)
+    spath = os.path.dirname(os.path.abspath(__file__))
+    sname = 'hextob58.js'
+    js_path = os.path.join(spath, sname)
+
+    response = muterun_js(js_path, hexstr)
     if response.exitcode == 0:
         return response.stdout.decode().strip()
     else:
