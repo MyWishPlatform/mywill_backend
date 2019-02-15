6a15636d44517994b5a3e9a9e56d2bc422ceb9a3
Maksim Strenk <ephdtrg@gmail.com>
2019-02-15 17:46:56 +0300

    add tron lost key model

--- lastwill/contracts/submodels/tron.py
+++ lastwill/contracts/submodels/tron.py
@@ -550,3 +550,139 @@ class ContractDetailsTRONAirdrop(CommonDetails):
         self.contract.state = 'ACTIVE'
         self.contract.save()
         take_off_blocking(self.contract.network.name)
+
+@contract_details('Tron Lost key contract')
+class ContractDetailsTRONLostKey(CommonDetails):
+    sol_path = 'lastwill/lost-key/'
+    source_filename = 'contracts/LostKeyDelayedPaymentWallet.sol'
+    result_filename = 'build/contracts/LostKeyDelayedPaymentWallet.json'
+    user_address = models.CharField(max_length=50, null=True, default=None)
+    check_interval = models.IntegerField()
+    active_to = models.DateTimeField()
+    last_check = models.DateTimeField(null=True, default=None)
+    next_check = models.DateTimeField(null=True, default=None)
+    eth_contract = models.ForeignKey(EthContract, null=True, default=None)
+    transfer_threshold_wei = models.IntegerField(default=0)
+    transfer_delay_seconds = models.IntegerField(default=0)
+
+    def predeploy_validate(self):
+        now = timezone.now()
+        if self.active_to < now:
+            raise ValidationError({'result': 1}, code=400)
+
+    def get_arguments(self, *args, **kwargs):
+        return [
+            self.user_address,
+            [h.address for h in self.contract.heir_set.all()],
+            [h.percentage for h in self.contract.heir_set.all()],
+            self.check_interval,
+#            self.transfer_threshold_wei,
+#            self.transfer_delay_seconds
+            2**256-1,
+            0,
+        ]
+
+    def fundsAdded(self, message):
+        pass
+
+    @classmethod
+    def min_cost(cls):
+        network = Network.objects.get(name='ETHEREUM_MAINNET')
+        now = datetime.datetime.now()
+        cost = cls.calc_cost({
+            'check_interval': 1,
+            'heirs': [],
+            'active_to': now
+        }, network)
+        return cost
+
+    @staticmethod
+    def calc_cost(kwargs, network):
+        if NETWORKS[network.name]['is_free']:
+            return 0
+        heirs_num = int(kwargs['heirs_num']) if 'heirs_num' in kwargs else len(
+            kwargs['heirs'])
+        active_to = kwargs['active_to']
+        if isinstance(active_to, str):
+            if 'T' in active_to:
+                active_to = active_to[:active_to.index('T')]
+            active_to = datetime.date(*map(int, active_to.split('-')))
+        elif isinstance(active_to, datetime.datetime):
+            active_to = active_to.date()
+        check_interval = int(kwargs['check_interval'])
+        Cg = 1476117
+        CBg = 28031
+        Tg = 22000
+        Gp = 60 * NET_DECIMALS['ETH_GAS_PRICE']
+        Dg = 29435
+        DBg = 9646
+        B = heirs_num
+        Cc = 124852
+        DxC = max(abs((
+                                  datetime.date.today() - active_to).total_seconds() / check_interval),
+                  1)
+        O = 25000 * NET_DECIMALS['ETH_GAS_PRICE']
+        return 2 * int(
+            Tg * Gp + Gp * (Cg + B * CBg) + Gp * (Dg + DBg * B) + (
+                        Gp * Cc + O) * DxC
+        ) + 80000
+
+    @postponable
+    @check_transaction
+    def msg_deployed(self, message):
+        super().msg_deployed(message)
+        self.next_check = timezone.now() + datetime.timedelta(
+            seconds=self.check_interval)
+        self.save()
+
+    @check_transaction
+    def checked(self, message):
+        now = timezone.now()
+        self.last_check = now
+        next_check = now + datetime.timedelta(seconds=self.check_interval)
+        if next_check < self.active_to:
+            self.next_check = next_check
+        else:
+            self.contract.state = 'EXPIRED'
+            self.contract.save()
+            self.next_check = None
+        self.save()
+        take_off_blocking(self.contract.network.name, self.contract.id)
+
+    @check_transaction
+    def triggered(self, message):
+        self.last_check = timezone.now()
+        self.next_check = None
+        self.save()
+        heirs = Heir.objects.filter(contract=self.contract)
+        link = NETWORKS[self.eth_contract.contract.network.name]['link_tx']
+        for heir in heirs:
+            if heir.email:
+                send_mail(
+                    heir_subject,
+                    heir_message.format(
+                        user_address=heir.address,
+                        link_tx=link.format(tx=message['transactionHash'])
+                    ),
+                    DEFAULT_FROM_EMAIL,
+                    [heir.email]
+                )
+        self.contract.state = 'TRIGGERED'
+        self.contract.save()
+        if self.contract.user.email:
+            send_mail(
+                carry_out_subject,
+                carry_out_message,
+                DEFAULT_FROM_EMAIL,
+                [self.contract.user.email]
+            )
+
+    def get_gaslimit(self):
+        Cg = 3200000
+        CBg = 28031
+        return Cg + len(self.contract.heir_set.all()) * CBg
+
+    @blocking
+    @postponable
+    def deploy(self):
+        return super().deploy()
