7bbe80e1e536a2f2ca712073e4a2727623ef375c
karriva <nina.m.lukina@gmail.com>
2019-02-22 12:33:07 +0300

    add method got getting tron tokens in testnet for user

--- lastwill/contracts/api.py
+++ lastwill/contracts/api.py
@@ -967,7 +967,7 @@ def get_authio_cost(request):
 @api_view(http_method_names=['GET'])
 def get_testnet_tron_tokens(request):
     user = request.user
-    contracts = Contract.objects.filter(user=user, contract_type=15, network_name='TRON_TESTNET')
+    contracts = Contract.objects.filter(user=user, contract_type=15, network__name='TRON_TESTNET')
     answer = []
     for c in contracts:
         d = c.get_details()
