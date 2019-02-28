29ec6384198a6f7f9cd1229f66504903fbd8e9f6
karriva <nina.m.lukina@gmail.com>
2019-02-28 17:42:00 +0300

    add new contract

--- lastwill/contracts/submodels/lostkey.py
+++ lastwill/contracts/submodels/lostkey.py
@@ -146,3 +146,165 @@ class ContractDetailsLostKey(CommonDetails):
     @postponable
     def deploy(self):
         return super().deploy()
+
+
+@contract_details('Wallet contract (lost key)')
+class ContractDetailsLostKeyTokens(CommonDetails):
+    user_address = models.CharField(max_length=50, null=True, default=None)
+    check_interval = models.IntegerField()
+    active_to = models.DateTimeField()
+    last_check = models.DateTimeField(null=True, default=None)
+    next_check = models.DateTimeField(null=True, default=None)
+    temp_directory = models.CharField(max_length=36)
+    eth_contract = models.ForeignKey(
+        EthContract,
+        null=True,
+        default=None,
+        related_name='eth_lostkey_details',
+        on_delete=models.SET_NULL
+    )
+    email = models.CharField(max_length=256, null=True, default=None)
+    platform_alive = models.BooleanField(default=False)
+    platform_cancel = models.BooleanField(default=False)
+    last_reset = models.DateTimeField(null=True, default=None)
+    last_press_imalive = models.DateTimeField(null=True, default=None)
+
+    def predeploy_validate(self):
+        pass
+
+    def get_arguments(self, *args, **kwargs):
+        return [
+            self.user_address,
+            [h.address for h in self.contract.heir_set.all()],
+            [h.percentage for h in self.contract.heir_set.all()],
+            self.check_interval,
+            False if self.contract.network.name in
+                     ['ETHEREUM_MAINNET', 'ETHEREUM_ROPSTEN'] else True,
+        ]
+
+    @classmethod
+    def min_cost(cls):
+        network = Network.objects.get(name='ETHEREUM_MAINNET')
+        now = datetime.datetime.now()
+        cost = cls.calc_cost({
+            'check_interval': 1,
+            'heirs': [],
+            'active_to': now
+        }, network)
+        return cost
+
+    @staticmethod
+    def calc_cost(kwargs, network):
+        if NETWORKS[network.name]['is_free']:
+            return 0
+        result = int(0.5 * 10 ** 18)
+        return result
+
+    @postponable
+    @check_transaction
+    def msg_deployed(self, message):
+        super().msg_deployed(message)
+        self.next_check = timezone.now() + datetime.timedelta(
+            seconds=self.check_interval)
+        self.save()
+
+    @check_transaction
+    def checked(self, message):
+        now = timezone.now()
+        self.last_check = now
+        next_check = now + datetime.timedelta(seconds=self.check_interval)
+        if next_check < self.active_to:
+            self.next_check = next_check
+        else:
+            self.contract.state = 'EXPIRED'
+            self.contract.save()
+            self.next_check = None
+        self.save()
+        take_off_blocking(self.contract.network.name, self.contract.id)
+
+    @check_transaction
+    def triggered(self, message):
+        self.last_check = timezone.now()
+        self.next_check = None
+        self.save()
+        heirs = Heir.objects.filter(contract=self.contract)
+        link = NETWORKS[self.eth_contract.contract.network.name]['link_tx']
+        for heir in heirs:
+            if heir.email:
+                send_mail(
+                    heir_subject,
+                    heir_message.format(
+                        user_address=heir.address,
+                        link_tx=link.format(tx=message['transactionHash'])
+                    ),
+                    DEFAULT_FROM_EMAIL,
+                    [heir.email]
+                )
+        self.contract.state = 'TRIGGERED'
+        self.contract.save()
+        if self.contract.user.email:
+            send_mail(
+                carry_out_subject,
+                carry_out_message,
+                DEFAULT_FROM_EMAIL,
+                [self.contract.user.email]
+            )
+
+    def get_gaslimit(self):
+        Cg = 3200000
+        CBg = 28031
+        return Cg + len(self.contract.heir_set.all()) * CBg
+
+    @blocking
+    @postponable
+    def deploy(self):
+        return super().deploy()
+
+    def compile(self, eth_contract_attr_name='eth_contract_token'):
+        print('tron lostkey contract compile')
+        if self.temp_directory:
+            print('already compiled')
+            return
+        dest, preproc_config = create_directory(
+            self, sour_path='lastwill/eth-lost-key-token/*',
+            config_name='c-preprocessor-config.json'
+        )
+        heirs = self.contract.heir_set.all()
+        heirs_list = ','.join(map(lambda h: 'address(%s)' % h.address, heirs))
+        heirs_percents = ','.join(map(lambda h: 'uint(%s)' % h.percentage, heirs))
+        preproc_params = {'constants': {}}
+        preproc_params["constants"]["D_TARGET"] = "0xf17f52151EbEF6C7334FAD080c5704D77216b732"
+        preproc_params["constants"]["D_HEIRS"] = heirs_list
+        preproc_params["constants"]["D_PERCENTS"] = heirs_percents
+        preproc_params["constants"]["D_PERIOD_SECONDS"] = self.check_interval
+        preproc_params["constants"]["D_HEIRS_COUNT"] = len(heirs)
+        print('params', preproc_params, flush=True)
+
+        with open(preproc_config, 'w') as f:
+            f.write(json.dumps(preproc_params))
+        if os.system('cd {dest} && yarn compile'.format(dest=dest)):
+            raise Exception('compiler for test error while deploying')
+        if os.system('cd {dest} && yarn test'.format(
+                dest=dest)):
+            raise Exception('testing error')
+
+        preproc_params["constants"]["D_TARGET"] = self.user_address
+        with open(preproc_config, 'w') as f:
+            f.write(json.dumps(preproc_params))
+        if os.system('cd {dest} && yarn compile'.format(dest=dest)):
+            raise Exception('compiler error while deploying')
+
+        with open(path.join(dest, 'build/contracts/LostKeyMain.json'), 'rb') as f:
+            token_json = json.loads(f.read().decode('utf-8-sig'))
+        with open(path.join(dest, 'build/LostKeyMain.sol'), 'rb') as f:
+            source_code = f.read().decode('utf-8-sig')
+        eth_contract = EthContract()
+        eth_contract.abi = token_json['abi']
+        eth_contract.bytecode = token_json['bytecode'][2:]
+        eth_contract.compiler_version = token_json['compiler']['version']
+        eth_contract.contract = self.contract
+        eth_contract.original_contract = self.contract
+        eth_contract.source_code = source_code
+        eth_contract.save()
+        self.eth_contract = eth_contract
+        self.save()
