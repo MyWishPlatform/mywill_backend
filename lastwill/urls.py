93746080a5606592b9aaeeceae8061937d2ade79
karriva <nina.m.lukina@gmail.com>
2019-03-04 16:10:06 +0300

    add url

--- lastwill/urls.py
+++ lastwill/urls.py
@@ -33,7 +33,7 @@ from lastwill.contracts.api import (ContractViewSet, get_code, test_comp,
                                     get_invest_balance_day, check_status,
                                     get_eos_cost, EOSAirdropAddressViewSet, get_eos_airdrop_cost,
                                     check_eos_accounts_exists, buy_brand_report, get_authio_cost,
-                                    get_testnet_tron_tokens)
+                                    get_testnet_tron_tokens, get_tokens_for_eth_address)
 from lastwill.contracts.api_eos import (create_eos_account, deploy_eos_account,
                                         show_eos_account, edit_eos_account,
                                         calculate_cost_eos_account, calculate_cost_eos_account_contract,
@@ -132,6 +132,7 @@ urlpatterns = [
     url(r'^api/get_contracts/$', get_contracts),
     url(r'^api/get_available_contracts/$', get_available_contracts),
     url(r'^api/get_testnet_tron_tokens/$', get_testnet_tron_tokens),
+    url(r'^api/get_tokens_for_eth_address/$', get_tokens_for_eth_address),
 ]
 
 urlpatterns += url(r'^/*', index, name='all'),
