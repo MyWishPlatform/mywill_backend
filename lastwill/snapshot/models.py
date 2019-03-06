87c4623f03f36208f277535d217f50df92f4df37
karriva <nina.m.lukina@gmail.com>
2019-03-06 13:19:34 +0300

    add tables for shapshot

--- lastwill/snapshot/models.py
+++ lastwill/snapshot/models.py
@@ -13,3 +13,24 @@ class SnapshotEOSRow(models.Model):
     value = models.DecimalField(
             max_digits=MAX_WEI_DIGITS, decimal_places=0, null=True
     )
+
+
+class TRONSnapshotEth(models.Model):
+    eth_address = models.CharField(max_length=50)
+    balance = models.DecimalField(
+            max_digits=MAX_WEI_DIGITS, decimal_places=0, null=True
+    )
+
+
+class TRONSnapshotEOS(models.Model):
+    eos_address = models.CharField(max_length=50)
+    balance = models.DecimalField(
+            max_digits=MAX_WEI_DIGITS, decimal_places=0, null=True
+    )
+
+
+class TRONSnapshotTRON(models.Model):
+    tron_address = models.CharField(max_length=50)
+    balance = models.DecimalField(
+            max_digits=MAX_WEI_DIGITS, decimal_places=0, null=True
+    )
