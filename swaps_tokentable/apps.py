4d0a7896b87be7a08b055b9a8fd65d7484ebcdd7
Maksim Strenk <ephdtrg@gmail.com>
2019-03-18 16:54:22 +0100

    add token table app for swaps contract

---
+++ swaps_tokentable/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SwapsTokentableConfig(AppConfig):
+    name = 'swaps_tokentable'
---
+++ swaps_tokentable/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class SwapsTokentable(models.Model):
+    address = models.CharField(max_length=50)
+    token_name = models.CharField(max_length=512)
+    token_short_name = models.CharField(max_length=64)
+    decimals = models.IntegerField()
+    token_name = models.CharField(max_length=512)
