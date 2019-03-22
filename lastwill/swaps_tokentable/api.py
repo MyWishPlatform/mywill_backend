75ff65e877350110d571629620ca4428e0b2ec51
karriva <nina.m.lukina@gmail.com>
2019-03-22 13:53:11 +0300

    edit method with filter tokens

--- lastwill/swaps_tokentable/api.py
+++ lastwill/swaps_tokentable/api.py
@@ -5,14 +5,15 @@ from lastwill.swaps_tokentable.models import Tokens
 
 @api_view()
 def get_all_tokens(request):
-    token_shortname = request.query_params.get('token_short_name', None)
+    token_short_name = request.query_params.get('token_short_name', None)
     token_name = request.query_params.get('token_name', None)
 
-    tokens_all = Tokens.objects.all()
-    token_list = tokens_all.filter(token_short_name__startswith=token_shortname.upper())
+    token_list = Tokens.objects.all()
+    if token_short_name:
+        token_list = token_list.filter(token_short_name__startswith=token_short_name.upper())
 
     if token_name:
-        token_list = token_list.filter(token_name=token_name)
+        token_list = token_list.filter(token_name__startswith=token_name)
 
     result = []
     for t in token_list:
