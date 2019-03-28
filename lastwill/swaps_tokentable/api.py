6286815043fe155a3614b78894439f4bbbde4599
karriva <nina.m.lukina@gmail.com>
2019-03-28 17:50:06 +0300

    add get test tokens for testnet

--- lastwill/swaps_tokentable/api.py
+++ lastwill/swaps_tokentable/api.py
@@ -1,12 +1,40 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+
 from lastwill.swaps_tokentable.models import Tokens
+from lastwill.contracts.models import *
+from lastwill.settings import DEFAULT_IMAGE_LINK
+
+
+def get_test_tokens(token_name=None, token_short_name=None):
+    token_list = ContractDetailsToken.objects.all().exclude(contract__state__in=('CREATED', 'POSTPONED'))
+    if token_short_name:
+        token_list = token_list.filter(
+            token_short_name__startswith=token_short_name.upper())
+
+    if token_name:
+        token_list = token_list.filter(token_name__istartswith=token_name)
+    result = []
+    for t in token_list:
+        result.append({
+            'address': t.address,
+            'token_name': t.token_name,
+            'token_short_name': t.token_short_name,
+            'decimals': t.decimals,
+            'image_link': DEFAULT_IMAGE_LINK
+        })
+    return result
 
 
 @api_view()
 def get_all_tokens(request):
+    host = request.META['HTTP_HOST']
     token_short_name = request.query_params.get('token_short_name', None)
     token_name = request.query_params.get('token_name', None)
+
+    if 'dev' in host.lower():
+        result = get_test_tokens(token_name, token_short_name)
+        return Response(result)
 
     token_list = Tokens.objects.all()
     if token_short_name:
