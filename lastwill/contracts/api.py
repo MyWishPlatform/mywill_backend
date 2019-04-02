88fd14e9ec1ca4fee831d1713590b4b8575a7f3e
karriva <nina.m.lukina@gmail.com>
2019-04-02 12:04:33 +0300

    bug fix

--- lastwill/contracts/api.py
+++ lastwill/contracts/api.py
@@ -1086,5 +1086,5 @@ def confirm_swaps_info(request):
         c.save()
     contract.state = 'WAITING_FOR_PAYMENT'
     contract.save()
-    autodeploing(request.user)
+    autodeploing(contract.user.id)
     return JsonResponse(ContractSerializer().to_representation(contract))
