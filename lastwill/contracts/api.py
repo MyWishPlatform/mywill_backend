3d64d0cbd1b1e6d3b88bf9fbf9e6e759e2b27a28
karriva <nina.m.lukina@gmail.com>
2019-04-05 13:07:47 +0300

    bugfix

--- lastwill/contracts/api.py
+++ lastwill/contracts/api.py
@@ -1096,7 +1096,7 @@ def get_contract_for_unique_link(request):
     if not link:
         raise PermissionDenied
     details = ContractDetailsSWAPS.objects.filter(unique_link=link).first()
-    if not details or not details.public:
+    if not details:
         raise PermissionDenied
     contract = details.contract
     return JsonResponse(ContractSerializer().to_representation(contract))
