cc087bc0d3cc4a68c24a610bdc63d8f7e902acce
karriva <nina.m.lukina@gmail.com>
2019-04-08 12:40:25 +0300

    add middleware for session add domain

--- lastwill/profile/middleware/session_middleware.py
+++ lastwill/profile/middleware/session_middleware.py
@@ -2,13 +2,14 @@ from django.conf import settings
 
 
 class CrossDomainSessionMiddleware(object):
-    def process_response(self, request, response):
-        if response.cookies:
-            host = request.get_host()
-            # check if it's a different domain
-            if host not in settings.SESSION_COOKIE_DOMAIN:
-                domain = ".{domain}".format(domain=host)
-                for cookie in response.cookies:
-                    if 'domain' in response.cookies[cookie]:
-                        response.cookies[cookie]['domain'] = domain
-        return response
+    pass
+    # def process_response(self, request, response):
+    #     if response.cookies:
+    #         host = request.get_host()
+    #         # check if it's a different domain
+    #         if host not in settings.SESSION_COOKIE_DOMAIN:
+    #             domain = ".{domain}".format(domain=host)
+    #             for cookie in response.cookies:
+    #                 if 'domain' in response.cookies[cookie]:
+    #                     response.cookies[cookie]['domain'] = domain
+    #     return response
