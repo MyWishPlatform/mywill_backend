9fef336ff872fe7b17561ff660d9953ff6571ef5
karriva <nina.m.lukina@gmail.com>
2019-04-08 12:50:49 +0300

    add middleware for session add domain

--- lastwill/settings.py
+++ lastwill/settings.py
@@ -68,7 +68,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'profile.middleware.session_middleware.CrossDomainSessionMiddleware'
+    'lastwill.profile.middleware.session_middleware.CrossDomainSessionMiddleware'
 ]
 
 ROOT_URLCONF = 'lastwill.urls'
