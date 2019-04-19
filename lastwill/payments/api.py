99f8aabc5d10e9c4e85c9093032d140536fda397
karriva <nina.m.lukina@gmail.com>
2019-04-19 14:00:15 +0300

    add convert bnb wish in payment

--- lastwill/payments/api.py
+++ lastwill/payments/api.py
@@ -18,6 +18,9 @@ def create_payment(uid, tx, currency, amount, site_id):
         return
     print('create payment')
     if SubSite.objects.get(id=site_id).site_name == MY_WISH_URL:
+        if currency == 'WISH-1EF':
+            currency = 'WISH'
+            amount = amount * 10 ** 10
         value = amount if currency == 'WISH' else to_wish(
             currency, amount
         )
