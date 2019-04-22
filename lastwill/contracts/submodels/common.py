353be34fcf967dd15b4d0a4f8d9ef645a18294a5
karriva <nina.m.lukina@gmail.com>
2019-04-22 13:14:51 +0300

    edit generate memo contract

--- lastwill/contracts/submodels/common.py
+++ lastwill/contracts/submodels/common.py
@@ -339,6 +339,7 @@ class Contract(models.Model):
         tron_lostkey = apps.get_model('contracts', 'ContractDetailsTRONLostkey')
         eth_lostkey_tokens = apps.get_model('contracts', 'ContractDetailsLostKeyTokens')
         swap = apps.get_model('contracts', 'ContractDetailsSWAPS')
+        swap2 = apps.get_model('contracts', 'ContractDetailsSWAPS2')
 
         contract_details_types[0] = {'name': 'Will contract', 'model': lastwill}
         contract_details_types[1] = {'name': 'Wallet contract (lost key)',
@@ -362,6 +363,7 @@ class Contract(models.Model):
         contract_details_types[18] = {'name': 'TRON LostKey', 'model': tron_lostkey}
         contract_details_types[19] = {'name': 'ETH LostKey with tokens', 'model': eth_lostkey_tokens}
         contract_details_types[20] = {'name': 'SWAPS Contract', 'model': swap}
+        contract_details_types[21] = {'name': 'SWAPS Contract', 'model': swap2}
         return contract_details_types
 
     @classmethod
