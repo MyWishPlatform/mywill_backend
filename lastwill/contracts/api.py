7e52394dcc8a002a4a73f9355f2a5bd2f3530abd
Maksim Strenk <ephdtrg@gmail.com>
2019-06-28 19:15:58 +0300

    fix details creation

--- lastwill/contracts/api.py
+++ lastwill/contracts/api.py
@@ -1206,7 +1206,7 @@ def create_swap2_for_events(order):
     order_details.pop('state')
     order_details.pop('id')
 
-    swap2_details = ContractDetailsSWAPS2Serializer.create(swap2_contract, order_details)
+    swap2_details = ContractDetailsSWAPS2Serializer().create(swap2_contract, order_details)
 
     swap2_contract.save()
     swap2_details.save()
