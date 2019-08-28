a72a9af11438d51ba95f7c319ee38302eac32355
Euphemia Deathridge <ephdtrg@gmail.com>
2019-08-28 13:02:28 +0300

    expand length of fields in cmc token table

--- lastwill/swaps_common/tokentable/models.py
+++ lastwill/swaps_common/tokentable/models.py
@@ -12,8 +12,8 @@ class Tokens(models.Model):
 class TokensCoinMarketCap(models.Model):
     token_cmc_id = models.IntegerField(null=True)
     token_name = models.CharField(max_length=512)
-    token_short_name = models.CharField(max_length=64)
-    token_platform = models.CharField(max_length=64)
-    token_address = models.CharField(max_length=64)
+    token_short_name = models.CharField(max_length=128)
+    token_platform = models.CharField(max_length=128)
+    token_address = models.CharField(max_length=128)
     image_link = models.CharField(max_length=512)
     token_rank = models.IntegerField(null=True, default=None)
