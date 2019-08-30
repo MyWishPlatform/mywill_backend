bdd26daebd74a22c304a5125b3e8e1b585f83b34
Euphemia Deathridge <ephdtrg@gmail.com>
2019-08-30 11:14:48 +0300

    add headers to order creation endpoint

--- lastwill/swaps_common/orderbook/api_exchange.py
+++ lastwill/swaps_common/orderbook/api_exchange.py
@@ -20,101 +20,98 @@ from lastwill.profile.models import *
 
 @api_view(http_method_names=['POST', 'OPTIONS'])
 def create_swaps_order_api(request):
-    token = request.META['SESSION_TOKEN']
-    if not token:
-        raise ValidationError({'result': 'Session token not found'}, code=404)
-
-    data = decode_session_token(token)
-
-    exchange_domain_name = data['exchange_domain']
-    exchange_account = User.objects.get(username=data['exchange_profile'])
-    if exchange_account.username != exchange_domain_name:
-        raise ValidationError('Domain name not matching username')
-
-    user_from_exchange = data['exchange_user']
-
-    base_coin_id = quote_coin_id = 0
-    base_address = quote_address = None
-
-    if 'base_coin_id' and 'quote_coin_id' in request.data:
-        base_coin_id = request.data['base_coin_id']
-        quote_coin_id = request.data['quote_coin_id']
-    elif 'base_address' and 'quote_address' in request.data:
-        base_address = request.data['base_address']
-        quote_address = request.data['quote_address']
+    session_token_headers = set_cors_headers('SESSION_TOKEN')
+    if request.method == 'OPTIONS':
+        return Response(status=200, headers=session_token_headers)
     else:
-        raise ValidationError('Required pairs of: base_coin_id and quote_coin_id or base_address and quote_adress')
+        token = request.META['HTTP_SESSION_TOKEN']
+        if not token:
+            raise ValidationError({'result': 'Session token not found'}, code=404)
 
-    if 'stop_date' in request.params:
-        stop_date = datetime.datetime.strptime(request.data['stop_date'], '%Y-%m-%d %H:%M')
-    else:
-        stop_date = datetime.datetime.now(timezone.utc) + datetime.timedelta(days=3)
+        data = decode_session_token(token)
 
-    link = ''.join(
-            random.choice(string.ascii_lowercase + string.digits) for _ in
-            range(6)
+        exchange_domain_name = data['exchange_domain']
+        exchange_account = User.objects.get(username=data['exchange_profile'])
+        if exchange_account.username != exchange_domain_name:
+            raise ValidationError('Domain name not matching username')
+
+        user_from_exchange = data['exchange_user']
+
+        base_coin_id = quote_coin_id = 0
+        base_address = quote_address = None
+
+        if 'base_coin_id' and 'quote_coin_id' in request.data:
+            base_coin_id = request.data['base_coin_id']
+            quote_coin_id = request.data['quote_coin_id']
+        elif 'base_address' and 'quote_address' in request.data:
+            base_address = request.data['base_address']
+            quote_address = request.data['quote_address']
+        else:
+            raise ValidationError('Required pairs of: base_coin_id and quote_coin_id or base_address and quote_adress')
+
+        if 'stop_date' in request.params:
+            stop_date = datetime.datetime.strptime(request.data['stop_date'], '%Y-%m-%d %H:%M')
+        else:
+            stop_date = datetime.datetime.now(timezone.utc) + datetime.timedelta(days=3)
+
+        link = ''.join(
+                random.choice(string.ascii_lowercase + string.digits) for _ in
+                range(6)
+            )
+
+        memo = '0x' + ''.join(random.choice('abcdef' + string.digits) for _ in range(64))
+
+        backend_contract = OrderBookSwaps(
+                name='exchange_order',
+                base_address=base_address,
+                base_limit=None,
+                base_coin_id=base_coin_id,
+                quote_address=quote_address,
+                quote_limit=None,
+                quote_coin_id=quote_coin_id,
+                owner_address=None,
+                stop_date=stop_date,
+                public=True,
+                unique_link=link,
+                user=exchange_account,
+                broker_fee=False,
+                memo_contract=memo,
+                comment=None,
+                min_base_wei=None,
+                min_quote_wei=None,
+                whitelist=False,
+                base_amount_contributed=0,
+                base_amount_total=0,
+                quote_amount_contributed=0,
+                quote_amount_total=0,
+                is_exchange=True,
+                exchange_user=user_from_exchange
         )
 
-    memo = '0x' + ''.join(random.choice('abcdef' + string.digits) for _ in range(64))
+        backend_contract.save()
 
+        backend_contract.state = 'ACTIVE'
 
-    backend_contract = OrderBookSwaps(
-            name='exchange_order',
-            base_address=base_address,
-            base_limit=None,
-            base_coin_id=base_coin_id,
-            quote_address=quote_address,
-            quote_limit=None,
-            quote_coin_id=quote_coin_id,
-            owner_address=None,
-            stop_date=stop_date,
-            public=True,
-            unique_link=link,
-            user=exchange_account,
-            broker_fee=False,
-            memo_contract=memo,
-            comment=None,
-            min_base_wei=None,
-            min_quote_wei=None,
-            whitelist=False,
-            base_amount_contributed=0,
-            base_amount_total=0,
-            quote_amount_contributed=0,
-            quote_amount_total=0,
-            is_exchange=True,
-            exchange_user=user_from_exchange
-    )
+        if not(base_address and quote_address):
+            backend_contract.contract_state = 'ACTIVE'
+        else:
+            backend_contract.contract_state = 'CREATED'
 
-    backend_contract.save()
+        backend_contract.save()
+        details = get_swap_from_orderbook(swap_id=backend_contract.id)
 
-    backend_contract.state = 'ACTIVE'
-
-    if not(base_address and quote_address):
-        backend_contract.contract_state = 'ACTIVE'
-    else:
-        backend_contract.contract_state = 'CREATED'
-
-    backend_contract.save()
-    details = get_swap_from_orderbook(swap_id=backend_contract.id)
-
-    print('sending swap order in queue ', backend_contract.id, flush=True)
-    send_in_queue(backend_contract.id, 'launch', SWAPS_ORDERBOOK_QUEUE)
-    return Response(details)
+        print('sending swap order in queue ', backend_contract.id, flush=True)
+        send_in_queue(backend_contract.id, 'launch', SWAPS_ORDERBOOK_QUEUE)
+        return Response(details, status=200, headers=session_token_headers)
 
 
 @xframe_options_exempt
 @api_view(http_method_names=['POST', 'OPTIONS'])
 def create_token_for_session(request):
-    cors_headers = {
-                'access-control-allow-methods': 'POST',
-                'access-control-allow-headers': 'Content-Type, TOKEN',
-                'access-control-allow-origin': '*',
-                'vary': 'Origin, Access-Control-Request-Method, Access-Control-Request-Headers'
-
-            }
-
+    additional_header = 'TOKEN'
+    token_headers = set_cors_headers('TOKEN')
     if request.method == 'OPTIONS':
-        return Response(status=200, headers=cors_headers)
+        return Response(status=200, headers=token_headers)
     else:
         api_key = request.META['HTTP_TOKEN']
         if not api_key:
@@ -130,7 +127,7 @@ def create_token_for_session(request):
         return Response(
                 data=data,
                 status=200,
-                headers=cors_headers
+                headers=token_headers
         )
 
 
@@ -161,3 +158,13 @@ def decode_session_token(token):
         return Response('Expired signature')
     except jwt.InvalidTokenError:
         return Response('Invalid token')
+
+
+def set_cors_headers(additional_header):
+    return {
+                'access-control-allow-methods': 'POST',
+                'access-control-allow-headers': 'Content-Type, {header}'.format(header=additional_header),
+                'access-control-allow-origin': '*',
+                'vary': 'Origin, Access-Control-Request-Method, Access-Control-Request-Headers'
+
+            }
