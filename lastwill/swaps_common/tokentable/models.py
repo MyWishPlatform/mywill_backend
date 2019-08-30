b0a66aa7f4d76a2b8cfb83ae03ad091be4cc45b4
Euphemia Deathridge <ephdtrg@gmail.com>
2019-08-30 14:48:36 +0300

    token platform can be None

--- lastwill/swaps_common/tokentable/models.py
+++ lastwill/swaps_common/tokentable/models.py
@@ -13,7 +13,7 @@ class TokensCoinMarketCap(models.Model):
     token_cmc_id = models.IntegerField(null=True)
     token_name = models.CharField(max_length=512)
     token_short_name = models.CharField(max_length=128)
-    token_platform = models.CharField(max_length=128)
+    token_platform = models.CharField(max_length=128, null=True)
     token_address = models.CharField(max_length=128)
     image_link = models.CharField(max_length=512)
     token_rank = models.IntegerField(null=True, default=None)
