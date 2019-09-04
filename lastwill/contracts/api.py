31d1bf90002a7e6427ef2dad1053b87bc15c56ea
Euphemia Deathridge <ephdtrg@gmail.com>
2019-09-04 18:45:22 +0300

    wrong class

--- lastwill/contracts/api.py
+++ lastwill/contracts/api.py
@@ -475,7 +475,7 @@ def get_usd_rub_rates():
     page = requests.get("https://www.fxempire.com/markets/usd-rub/overview")
     soup = BeautifulSoup(page.content,'html.parser')
     actual = (soup.find_all("div", class_='DirectionBackgroundColor__BackgroundColor-sc-1qjm64q-0 fgRxHG'))
-    course_change = (soup.find_all("span", class_="Span-sc-1abytr7-0 cddJEv"))
+    course_change = (soup.find_all("span", class_="Span-sc-1abytr7-0 hAkeNO"))
     actual_course = actual[0].get_text()
     course_change = course_change[0].get_text()[1:-2]
     rub_rate = {'base': 'USD', 'target': 'RUB', 'price': actual_course, 'change_24h': course_change}
