43867cf4851b9ecb6894a3a73695198e192bf3a9
Egor14 <egorgrekhnev@mail.ru>
2019-12-13 19:56:51 +0300

    some token protector logic

--- lastwill/contracts/submodels/token_protector.py
+++ lastwill/contracts/submodels/token_protector.py
@@ -1,20 +1,26 @@
 from lastwill.contracts.submodels.common import *
 from django.utils import timezone
 from rest_framework.exceptions import ValidationError
+import datetime
+from lastwill.consts import NET_DECIMALS, CONTRACT_GAS_LIMIT
 
 
 @contract_details('Token protrctor contract')
-class ContractDetailsTokenProtector():
+class ContractDetailsTokenProtector(CommonDetails):
     # test fields
-    sol_path = 'lastwill/lost-key/'
-    source_filename = 'contracts/LostKeyDelayedPaymentWallet.sol'
-    result_filename = 'build/contracts/LostKeyDelayedPaymentWallet.json'
+    sol_path = 'lastwill/token-protector/'
+    source_filename = 'contracts/TokenProtector.sol'
+    result_filename = 'build/contracts/TokenProtector.json'
     user_address = models.CharField(max_length=50, null=True, default=None)
+
     check_interval = models.IntegerField()
     active_to = models.DateTimeField()
+
     last_check = models.DateTimeField(null=True, default=None)
     next_check = models.DateTimeField(null=True, default=None)
+
     eth_contract = models.ForeignKey(EthContract, null=True, default=None)
+
     transfer_threshold_wei = models.IntegerField(default=0)
     transfer_delay_seconds = models.IntegerField(default=0)
 
@@ -23,56 +29,98 @@ class ContractDetailsTokenProtector():
         if self.active_to < now:
             raise ValidationError({'result': 1}, code=400)
 
-    def get_arguments(self, *args, **kwargs):
-        pass
-
-    def fundsAdded(self, message):
-        pass
-
-    @classmethod
-    def min_cost(cls):
-        pass
-
-    @staticmethod
-    def calc_cost(kwargs, network):
-        pass
-
     @postponable
     @check_transaction
     def msg_deployed(self, message):
-        pass
+        super().msg_deployed(message)
+        self.next_check = timezone.now() + datetime.timedelta(seconds=self.check_interval)
+        self.save()
 
-    @check_transaction
-    def checked(self, message):
-        pass
+    @classmethod
+    def min_cost(cls):
+        network = Network.objects.get(name='ETHEREUM_MAINNET')
+        cost = cls.calc_cost({}, network)
+        return cost
 
-    @check_transaction
-    def triggered(self, message):
-        pass
+    @staticmethod
+    def calc_cost(kwargs, network):
+        if NETWORKS[network.name]['is_free']:
+            return 0
+        return int(25 * NET_DECIMALS['USDT'])
+
 
     def get_gaslimit(self):
-        pass
+        return CONTRACT_GAS_LIMIT['TOKEN_PROTECTOR']
 
     @blocking
     @postponable
     def deploy(self):
-        pass
+        super().deploy()
 
-
-    def contractPayment(self, message):
-        pass
-
-    @blocking
-    def make_payment(self, message):
-        pass
-
-    @blocking
-    def cancel(self, message):
-        pass
 
     def compile(self, eth_contract_attr_name='eth_contract_token'):
         pass
 
+    @check_transaction
+    def checked(self, message):
+        now = timezone.now()
+        self.last_check = now
+        next_check = now + datetime.timedelta(seconds=self.check_interval)
+        if next_check < self.active_to:
+            self.next_check = next_check
+        else:
+            self.contract.state = 'EXPIRED'
+            self.contract.save()
+            self.next_check = None
+        self.save()
+        take_off_blocking(self.contract.network.name, self.contract.id)
 
-    def finalized(self, message):
-        pass
+    @check_transaction
+    def triggered(self, message):
+        link = NETWORKS[self.eth_contract.contract.network.name]['link_tx']
+        if self.recepient_email:
+            send_mail(
+                heir_subject,
+                heir_message.format(
+                    user_address=self.recepient_address,
+                    link_tx=link.format(tx=message['transactionHash'])
+                ),
+                DEFAULT_FROM_EMAIL,
+                [self.recepient_email]
+            )
+        self.contract.state = 'TRIGGERED'
+        self.contract.save()
+        if self.contract.user.email:
+            send_mail(
+                carry_out_subject,
+                carry_out_message,
+                DEFAULT_FROM_EMAIL,
+                [self.contract.user.email]
+            )
+
+    def get_arguments(self, *args, **kwargs):
+        return [
+            self.user_address,
+            # self.recepient_address,
+            2 ** 256 - 1,
+            int(self.date.timestamp()),
+        ]
+
+    # def contractPayment(self, message):
+    #     pass
+    #
+    # @blocking
+    # def make_payment(self, message):
+    #     pass
+
+
+    # @blocking
+    # def i_am_alive(self, message):
+    #     pass
+    #
+    # @blocking
+    # def cancel(self, message):
+    #     pass
+    #
+    # def fundsAdded(self, message):
+    #     pass
--- lastwill/contracts/token_protector_serializer.py
+++ lastwill/contracts/token_protector_serializer.py
@@ -1,7 +1,30 @@
 from lastwill.contracts.models import ContractDetailsTokenProtector
 from rest_framework import serializers
+from lastwill.contracts.serializers import EthContractSerializer
+
 
 class TokenSaverSerializer(serializers.ModelSerializer):
     class Meta:
         model = ContractDetailsTokenProtector
         fields = '__all__'
+
+    def to_representation(self, contract_details):
+        res = super().to_representation(contract_details)
+        res['eth_contract'] = EthContractSerializer().to_representation(contract_details.eth_contract)
+        if contract_details.contract.network.name in ['ETHEREUM_ROPSTEN', 'RSK_TESTNET']:
+            res['eth_contract']['source_code'] = ''
+        return res
+
+    def create(self, contract, contract_details):
+        kwargs = contract_details.copy()
+        kwargs['contract'] = contract
+        return super().create(kwargs)
+
+    def update(self, contract, details, contract_details):
+        kwargs = contract_details.copy()
+        kwargs['contract'] = contract
+        return super().update(details, kwargs)
+
+    def validate(self, contract_details):
+
+        pass
