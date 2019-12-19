5691ab39a3a7d91ffeef90825a6f25ac0aa771fd
Egor14 <egorgrekhnev@mail.ru>
2019-12-19 17:04:05 +0300

    del temp_directory

--- lastwill/contracts/submodels/token_protector.py
+++ lastwill/contracts/submodels/token_protector.py
@@ -61,9 +61,7 @@ class ContractDetailsTokenProtector(CommonDetails):
 
     def compile(self, eth_contract_attr_name='eth_contract_token'):
         print('token protector compiling', flush=True)
-        # if self.temp_directory:
-        #     print('already compiled', flush=True)
-        #     return
+
         dest, preproc_config = create_directory(
             self, sour_path='lastwill/token_saver/*',
             config_name='c-preprocessor-config.json'
