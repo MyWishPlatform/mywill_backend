ee98b635b5ae0e9b2b95a53a5b34d0505489d392
Egor14 <egorgrekhnev@mail.ru>
2020-01-24 18:44:12 +0300

    cost fix

--- lastwill/contracts/serializers.py
+++ lastwill/contracts/serializers.py
@@ -86,7 +86,7 @@ def deploy_swaps(contract_id):
         site_id = 4
         currency = 'USDT'
         user_info = UserSiteBalance.objects.get(user=contract.user, subsite__id=4)
-        if user_info.balance >= cost or int(user_info.balance) * 0.95 >= cost:
+        if user_info.balance >= cost or int(user_info.balance) >= cost * 0.95:
             create_payment(contract.user.id, '', currency, -cost, site_id, 'ETHEREUM_MAINNET')
             contract.state = 'WAITING_FOR_DEPLOYMENT'
             contract.save()
@@ -107,7 +107,7 @@ def deploy_protector(contract_id):
         site_id = 5
         currency = 'USDT'
         user_info = UserSiteBalance.objects.get(user=contract.user, subsite__id=5)
-        if user_info.balance >= cost or int(user_info.balance) * 0.95 >= cost:
+        if user_info.balance >= cost or int(user_info.balance) >= cost * 0.95:
             create_payment(contract.user.id, '', currency, -cost, site_id, 'ETHEREUM_MAINNET')
             contract.state = 'WAITING_FOR_DEPLOYMENT'
             contract.save()
