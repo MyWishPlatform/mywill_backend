0841c3688bd358a155f45500d0dbb3e3a8ed6271
Euphemia Deathridge <ephdtrg@mintyclouds.in>
2020-02-19 18:09:05 +0300

    set blocking mode for tron token

--- lastwill/contracts/submodels/tron.py
+++ lastwill/contracts/submodels/tron.py
@@ -1,5 +1,6 @@
 import datetime
 import base58
+import fcntl
 
 from django.db import models
 from django.core.mail import send_mail, EmailMessage
@@ -177,6 +178,7 @@ class ContractDetailsTRONToken(CommonDetails):
             owner_address='41' + convert_address_to_hex(NETWORKS[self.contract.network.name]['address'])[2:],
             origin_energy_limit=100000000
         )
+        fcntl.fcntl(1, fcntl.F_SETFL, 0)
 
         print('deployed contract: ', res, flush=True)
 
