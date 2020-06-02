2b5af67eb9301d2c0b91857a37a431b557c7acaa
Egor14 <egorgrekhnev@mail.ru>
2020-06-02 19:22:10 +0300

    trx freeze fix

--- freeze_trx.py
+++ freeze_trx.py
@@ -12,13 +12,14 @@ from lastwill.contracts.submodels.tron import instantiate_tronapi
 
 
 if __name__ == '__main__':
-    nets = [net for net in NETWORKS.keys() if 'TRON' in net]
     while True:
+        nets = [net for net in NETWORKS.keys() if 'TRON' in net]
+        print('nets', nets, flush=True)
         time.sleep(TRX_FREEZE_TIMEOUT)
         for net in nets:
             tron = instantiate_tronapi(NETWORKS[net]['private_key'], net)
-            unfreeze_tx = tron.trx.unfreeze('ENERGY')
+            unfreeze_tx = tron.trx.unfreeze_balance('ENERGY')
             print(net, 'unfreeze_tx', unfreeze_tx, flush=True)
             time.sleep(10)
-            freeze_tx = tron.trx.freeze(TRX_FREEZE_AMOUNT, 3, 'ENERGY')
+            freeze_tx = tron.trx.freeze_balance(TRX_FREEZE_AMOUNT, 3, 'ENERGY')
             print(net, 'freeze_tx', freeze_tx, flush=True)
