901855986c866a7150ad186702a882feb9a3511f
Egor14 <egorgrekhnev@mail.ru>
2020-06-11 12:44:53 +0300

    trx freeze pretty print

--- freeze_trx.py
+++ freeze_trx.py
@@ -1,5 +1,6 @@
 import time
 import os
+import datetime
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lastwill.settings')
@@ -14,12 +15,12 @@ from lastwill.contracts.submodels.tron import instantiate_tronapi
 if __name__ == '__main__':
     while True:
         nets = [net for net in NETWORKS.keys() if 'TRON' in net]
-        print('nets', nets, flush=True)
+        print('nets', nets, '\n', flush=True)
         time.sleep(TRX_FREEZE_TIMEOUT)
         for net in nets:
             tron = instantiate_tronapi(NETWORKS[net]['private_key'], net)
             unfreeze_tx = tron.trx.unfreeze_balance('ENERGY')
-            print(net, 'unfreeze_tx', unfreeze_tx, flush=True)
+            print(net, 'unfreeze_tx at', datetime.datetime.now(), '\n', unfreeze_tx, '\n', flush=True)
             time.sleep(10)
             freeze_tx = tron.trx.freeze_balance(TRX_FREEZE_AMOUNT, 3, 'ENERGY')
-            print(net, 'freeze_tx', freeze_tx, flush=True)
+            print(net, 'freeze_tx at', datetime.datetime.now(), '\n', freeze_tx, '\n', flush=True)
