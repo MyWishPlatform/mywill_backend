93db88fa146287ce0679b71f639faeed8332905d
Euphemia Deathridge <ephdtrg@mintyclouds.in>
2020-09-22 13:09:12 +0300

    add user ids to payment api

--- lastwill/payments/api.py
+++ lastwill/payments/api.py
@@ -163,6 +163,7 @@ def get_payment_statistics(start, stop=None):
         delta__gte=0, datetime__gte=start, datetime__lte=stop
     ).order_by('datetime')
     total_payments = {'ETH': 0.0, 'WISH': 0.0, 'BTC': 0.0, 'BNB': 0.0, 'EOS': 0.0, 'EOSISH': 0.0, 'TRX': 0.0, 'TRONISH': 0.0, 'BWISH': 0.0, 'SWAP': 0.0, 'OKB': 0.0}
+    user_ids = []
     for pay in payments:
         print(
             pay.datetime.date(),
@@ -173,5 +174,7 @@ def get_payment_statistics(start, stop=None):
             flush=True
         )
         total_payments[pay.original_currency] += float(pay.original_delta)/NET_DECIMALS[pay.original_currency]
+        user_ids.append(pay.user.id)
 
     print('total_payments', total_payments, flush=True)
+    return user_ids
