2b15e2455d4cb6e90364e0e7d12bcd529014a37e
Daniil Svetlov <dankasvetlov@gmail.com>
2021-01-26 17:22:41 +0300

    try to change output format to status and time

--- lastwill/panama_bridge/serializers.py
+++ lastwill/panama_bridge/serializers.py
@@ -1,9 +1,13 @@
-from rest_framework.serializers import ModelSerializer
+import re
+from rest_framework.serializers import ModelSerializer, DateField, SerializerMethodField
 
 from .models import PanamaTransaction
 
 
 class UserTransactionSerializer(ModelSerializer):
+    status = SerializerMethodField('change_status_view')
+    updateTime = DateField(format="%Y-%m-%d %H:%M")
+
     class Meta:
         model = PanamaTransaction
         fields = (
@@ -20,3 +24,5 @@ class UserTransactionSerializer(ModelSerializer):
             'walletToAddress',
             'walletDepositAddress',
         )
+    def change_status_view(self, obj):
+        return  re.sub(r"(\w)([A-Z])", r"\1 \2", self.status).capitalize()
