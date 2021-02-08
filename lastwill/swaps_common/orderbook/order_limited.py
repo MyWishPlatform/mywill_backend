d4f835cc20280d2f49ded637edb01514a987f081
Svetlov Daniil <svetlovdaniil@Svetlovs-MacBook-Air.local>
2021-02-08 13:19:27 +0300

    add contractABI

--- lastwill/swaps_common/orderbook/order_limited.py
+++ lastwill/swaps_common/orderbook/order_limited.py
@@ -1,8 +1,16 @@
 import requests
+import json
+import os
+from web3 import Web3, HTTPProvider
 
 COINGECKO_API = "https://api.coingecko.com"
 ETHERSCAN_API = "https://api.etherscan.io/api"
 ETHERSCAN_API_KEY = "D8QKZPVM9BMRWS7BY41RU9EKU2VMWT8PM5"
+
+INFURA_URL = 'https://mainnet.infura.io/v3/519bcee159504883ad8af59830dec2bb'
+WALLET_ADDRESS = '0xfCf49f25a2D1E49631d05614E2eCB45296F26258'
+CONTRACT_ADDRESS = '0xAAaCFf66942df4f1e1cB32C21Af875AC971A8117'
+BLOCKCHAIN_DECIMALS = 10 ** 18
 
 
 def get_rbc_eth_price():
@@ -60,7 +68,32 @@ def is_orderbook_profitable(exchangeRate, gasPrice, rbcValue, ethValue, isRBC):
         return False
 
 
+def test_calling():
+    print(get_rbc_eth_price())
+    print(get_gas_price())
 
-# test calling
-print(get_rbc_eth_price())
-print(get_gas_price())
+
+def get_abi_by_filename(filename):
+    build_dir = os.path.join(os.getcwd(), 'lastwill/swaps_common/orderbook/')
+
+    with open(os.path.join(build_dir, filename), 'r') as contract:
+        return json.load(contract)
+
+
+w3 = Web3(HTTPProvider(INFURA_URL))
+
+
+
+print(w3.isConnected())
+print(w3.eth.getBalance(WALLET_ADDRESS)/BLOCKCHAIN_DECIMALS)
+
+abi = get_abi_by_filename("contract_abi.json")
+contract = w3.eth.contract(address=CONTRACT_ADDRESS, abi=abi)
+
+function = contract.functions.myWishBasePercent()
+gas_limit = function.estimateGas({'from': CONTRACT_ADDRESS})
+
+print(gas_limit)
+
+#     tx_crypto_price = gas_limit * w3.eth.gasPrice / BLOCKCHAIN_DECIMALS
+
