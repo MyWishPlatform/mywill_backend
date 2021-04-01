7929833edb941981127572e03b929843c99e747a
yulian-khalitov <khalitov.yulian@yandex.ru>
2021-04-01 13:45:48 +0300

    fix: typo

--- lastwill/contracts/submodels/common.py
+++ lastwill/contracts/submodels/common.py
@@ -290,8 +290,8 @@ class Contract(models.Model):
     )
 
     created_date = models.DateTimeField(auto_now_add=True)
-    deployed_at = models.DateTimeField(null=True, default=True)
-    postponed_at = models.DateTimeField(null=True, default=True)
+    deployed_at = models.DateTimeField(null=True, default=None)
+    postponed_at = models.DateTimeField(null=True, default=None)
 
     check_interval = models.IntegerField(null=True, default=None)
     active_to = models.DateTimeField(null=True, default=None)
