b6c4e8907dda9d3d96cdb17db87d2e4348cadb35
maximepifanov <epifanov.maxim2014@yandex.ru>
2021-06-08 19:50:37 +0300

    Xinfin verification

--- lastwill/contracts/serializers.py
+++ lastwill/contracts/serializers.py
@@ -747,7 +747,7 @@ class ContractDetailsTokenSerializer(serializers.ModelSerializer):
 
     def create(self, contract, contract_details):
         if contract_details.admin_address[0:3] is not 'xdc':
-            contract_details.admin_address.replace('xdc', 'x0')
+            contract_details.admin_address.replace('xdc', '0x')
         token_holders = contract_details.pop('token_holders')
         for th_json in token_holders:
             th_json['address'] = th_json['address'].lower()
