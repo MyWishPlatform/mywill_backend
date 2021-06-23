54c5f12657167d3f20653eba94d637bc55a9024c
Pechenuca <maxim.epifanov8@gmail.com>
2021-06-23 17:37:39 +0300

    email

--- lastwill/contracts/submodels/common.py
+++ lastwill/contracts/submodels/common.py
@@ -553,7 +553,6 @@ class CommonDetails(models.Model):
         self.contract.state = 'ACTIVE'
         self.contract.deployed_at = datetime.datetime.now()
         self.contract.save()
-
         if self.contract.user.email:
             if self.contract.contract_type == 11:
                 send_mail(
