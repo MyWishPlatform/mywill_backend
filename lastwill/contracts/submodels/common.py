20722e2363c0fbd36e9bd10f5e82ce83bc5246e6
Kirill Bergman <40841278+orionoiro@users.noreply.github.com>
2021-06-28 12:15:14 +0300

    feat: Gas Station (#57)
    * gas station api
    
    * renamed imports
    
    * added logic
    
    * fixed decimals
    
    * update to use cached api
    
    * fuck the zero
    
    * hotfix
    
    * very well then

--- lastwill/contracts/submodels/common.py
+++ lastwill/contracts/submodels/common.py
@@ -3,6 +3,7 @@ import os
 import uuid
 import binascii
 import pika
+import requests
 from copy import deepcopy
 from base58 import b58decode
 from ethereum import abi
@@ -19,6 +20,7 @@ from neocore.Cryptography.Crypto import Crypto
 from neocore.UInt160 import UInt160
 
 from lastwill.settings import SIGNER, CONTRACTS_DIR, CONTRACTS_TEMP_DIR, WEB3_ATTEMPT_COOLDOWN
+from lastwill.settings import GAS_API_URL, SPEEDLVL
 from lastwill.parint import *
 from lastwill.consts import MAX_WEI_DIGITS, MAIL_NETWORK, ETH_COMMON_GAS_PRICES, NET_DECIMALS
 from lastwill.deploy.models import Network
@@ -483,6 +485,14 @@ class CommonDetails(models.Model):
             print(f'attempt {attempt} to get a nonce', flush=True)
             try:
                 nonce = int(eth_int.eth_getTransactionCount(address, "latest"), 16)
+                if self.contract.network.name == 'ETHEREUM_MAINNET':
+                    try:
+                        response = requests.get(GAS_API_URL).json()
+                        gas_price_current = response[SPEEDLVL] / 10
+                        gas_price_current = gas_price_current * 10 ** 9
+                        break
+                    except (requests.RequestException, KeyError):
+                        print('gas station api is unavailable', flush=True)
                 gas_price_current = int(1.1 * int(eth_int.eth_gasPrice(), 16))
                 break
 
