8349a822996b039776b434b9ed2ab8eb163a95d7
Kirill Bergman <k.bergman@protonmail.com>
2021-07-01 20:26:39 +0300

    chainId hotfix

--- lastwill/contracts/submodels/common.py
+++ lastwill/contracts/submodels/common.py
@@ -230,7 +230,7 @@ def send_in_queue(contract_id, type, queue):
     connection.close()
 
 
-def sign_transaction(address, nonce, gaslimit, value=None, dest=None, contract_data=None, gas_price=None):
+def sign_transaction(address, nonce, gaslimit, value=None, dest=None, contract_data=None, gas_price=None, chainId=None):
     data = {
         'from': address,
         'nonce': nonce,
@@ -244,6 +244,8 @@ def sign_transaction(address, nonce, gaslimit, value=None, dest=None, contract_d
         data['data'] = contract_data
     if gas_price:
         data['gasPrice'] = gas_price
+    if chainId:
+        data['chainId'] = chainId
 
     auth = HTTPSignatureAuth(key=SECRET_KEY, key_id=KEY_ID)
     signed_data = json.loads(requests.post(SIGNER, auth=auth, json=data).content.decode())
@@ -520,8 +522,9 @@ class CommonDetails(models.Model):
 
         gas_price_fixed = ETH_COMMON_GAS_PRICES[self.contract.network.name] * NET_DECIMALS['ETH_GAS_PRICE']
         gas_price = gas_price_current if gas_price_current < gas_price_fixed else gas_price_fixed
+        chainId = eth_int.eth_chainId()
         signed_data = sign_transaction(address, nonce, self.get_gaslimit(),
-                                       value=self.get_value(), contract_data=data, gas_price=gas_price)
+                                       value=self.get_value(), contract_data=data, gas_price=gas_price, chainId=chainId)
 
         print('fields of transaction', flush=True)
         print('source', address, flush=True)
