ae016be9f4287964c3899917060258be42916a65
Kirill Bergman <k.bergman@protonmail.com>
2021-07-21 22:54:06 +0300

    possible fix for import?

--- lastwill/telegram_bot/start_bot.py
+++ lastwill/telegram_bot/start_bot.py
@@ -1,3 +1,13 @@
+import os
+import sys
+
+
+sys.path.append(os.path.abspath(os.path.join(__file__, *[os.pardir] * 3)))
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bridge.settings')
+import django
+django.setup()
+
+
 from lastwill.settings import bot_token
 from lastwill.telegram_bot.bot import Bot
 
