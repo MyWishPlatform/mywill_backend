ef123b982dd223dc9434819098eeccf52e7822e4
Kirill Bergman <k.bergman@protonmail.com>
2021-07-28 16:02:50 +0300

    fixed loading token

--- lastwill/telegram_bot/main_bot.py
+++ lastwill/telegram_bot/main_bot.py
@@ -7,13 +7,14 @@ import time
 import telebot
 from django.db import IntegrityError
 
-from lastwill.settings import bot_token
 from .models import BotSub
 
 sys.path.append(os.path.abspath(os.path.join(__file__, *[os.pardir] * 3)))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lastwill.settings')
 import django
 django.setup()
+
+from lastwill.settings import bot_token
 
 
 class Bot(threading.Thread):
