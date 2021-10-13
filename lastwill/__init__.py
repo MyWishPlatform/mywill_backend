6b764ee8d44511c7a594bad077f6630ae5f4e407
Kirill Bergman <k.bergman@protonmail.com>
2021-10-13 23:22:58 +0300

    moved setup for cli in __init__

--- lastwill/__init__.py
+++ lastwill/__init__.py
@@ -1,5 +1,25 @@
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.
+from subprocess import Popen, PIPE
 from .celery import app as celery_app
+from .settings import BASE_DIR, SOLANA_CLI_DIR, NETWORKS
 
 __all__ = ('celery_app',)
+
+# Setup Solana cli
+setup_keypair = Popen([f'./solana config set --keypair {BASE_DIR}/keypair.json'],
+                      stdin=PIPE, stdout=PIPE, stderr=PIPE, cwd=SOLANA_CLI_DIR, shell=True)
+
+setup_network = Popen([f'./solana config set --url {NETWORKS["SOLANA_TESTNET"]["node_url"]}'],
+                      stdin=PIPE, stdout=PIPE, stderr=PIPE, cwd=SOLANA_CLI_DIR, shell=True)
+
+def setup():
+    for process in [setup_keypair, setup_network]:
+        stdout, stderr = process.communicate()
+        print(stdout.decode(), stderr.decode(), flush=True)
+        if process.returncode != 0:
+            raise Exception('encountered an error while setting up solana ')
+        print('successfully set up Solana cli ')
+    return True
+
+setup()
