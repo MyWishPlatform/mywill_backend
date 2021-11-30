8ca941a75cac78f2fae55688611acb2f5426b0a4
Kirill Bergman <k.bergman@protonmail.com>
2021-11-30 03:15:31 +0300

    fixes postgres

--- lastwill/settings.py
+++ lastwill/settings.py
@@ -101,13 +101,12 @@ WSGI_APPLICATION = 'lastwill.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'lastwill_new',
-        'USER': 'lastwill_new',
-        'PASSWORD': 'lastwill_new',
-        'HOST': 'localhost',
-        'PORT': 5432,
-        'CONN_MAX_AGE': None
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST', '127.0.0.1'),
+        'PORT': os.getenv('POSTGRES_PORT', 5432),
     }
 }
 
