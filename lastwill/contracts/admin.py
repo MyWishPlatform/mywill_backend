5182762209d27f2a91c124c5eceabc8344730490
lxndr <alex.loginov@mywish.io>
2022-04-28 14:05:58 +0300

    added search to Contract

--- lastwill/contracts/admin.py
+++ lastwill/contracts/admin.py
@@ -32,6 +32,7 @@ class CurrencyStatisticsCacheAdmin(admin.ModelAdmin):
 
 @admin.register(Contract)
 class ContractAdmin(admin.ModelAdmin):
+    search_fields = '__str__',
     list_display = '__str__', 'state', 'created_date', 'address', 'last_check', 'active_to'
 
 
