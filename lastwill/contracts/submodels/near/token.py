845ce26b93e382a00ed2d1d825cb655d296681a1
Alexander Loginov <alex.loginov@mywish.io>
2022-05-26 11:48:33 +0300

    Added compile()

--- lastwill/contracts/submodels/near/token.py
+++ lastwill/contracts/submodels/near/token.py
@@ -1,3 +1,6 @@
+from subprocess import run, STDOUT, PIPE, CalledProcessError
+from numpy import uint8
+from sys import byteorder
 import near_api
 from lastwill.contracts.submodels.common import *
 from lastwill.contracts.submodels.ico import AbstractContractDetailsToken
@@ -17,9 +20,14 @@ MAX_WEI_DIGITS_NEAR = 2 ** 128 - 1
 ADDRESS_LENGTH_NEAR = 64
 
 class BaseContract(Contract):
-    # address - адрес деплоя
-    # owner - адрес внутри контракта
-    # user - адрес подписи (должен совпадать с address или быть его родительским аккаунтом)
+    """
+    BaseContract - наследник базового контракта Contract из common.py
+
+    Args:
+        address - адрес деплоя
+        owner - адрес внутри контракта
+        user - адрес подписи (должен совпадать с address или быть его родительским аккаунтом)
+    """
     address = models.CharField(max_length=ADDRESS_LENGTH_NEAR, null=True, default=None)
     owner_address = models.CharField(max_length=ADDRESS_LENGTH_NEAR, null=True, default=None)
     user_address = models.CharField(max_length=ADDRESS_LENGTH_NEAR, null=True, default=None)
@@ -35,18 +43,47 @@ class NearContract(EthContract):
 @contract_details('Near Token contract')
 class ContractDetailsNearToken(AbstractContractDetailsToken):
     admin_address = models.CharField(max_length=ADDRESS_LENGTH_NEAR)
+    deploy_address = models.CharField(max_length=ADDRESS_LENGTH_NEAR, default='')
     token_type = models.CharField(max_length=32, default='NEP-141')
     maximum_supply = models.DecimalField(max_digits=MAX_WEI_DIGITS_NEAR, decimal_places=0, null=True)
-    eth_contract_token = models.ForeignKey(
-        EthContract,
+    near_contract = models.ForeignKey(
+        NearContract,
         null=True,
         default=None,
-        related_name='near_token_details_token',
+        related_name='near_token_details',
         on_delete=models.SET_NULL
     )
     
-    def compile(self, eth_contract_attr_name='eth_contract_token'):
-        pass
+    def compile(self):
+        if self.temp_directory:
+            print('Near Token is already compiled')
+            return
+        dest = create_directory(self, sour_path='lastwill/near_token/*', config_name=None)
+        try:
+            # https://docs.python.org/3/library/subprocess.html
+            result = run(['cd', f'{dest}', '&&', 'make'], stdout=PIPE, stderr=STDOUT, check=True)
+        except:
+            raise CalledProcessError(f'Near Token compile error\n', result.stdout.decode('utf-8'))
+        with open(path.join(dest, 'near.token/near.token.wasm'), 'rb') as f:
+            # код контракта представляет из себя побайтовый массив uint8
+            bytecode = []
+            while True:
+                char = f.read(1)
+                if not char:
+                    break
+            bytecode.append(uint8(int.from_bytes(char, byteorder)))
+        with open(path.join(dest, 'near.token.rs'), 'rb') as f:
+            # не уверен нужен ли вообще, но пока пусть будет так
+            source_code = f.read().decode('utf-8-sig')
+        near_contract = NearContract()
+        near_contract.abi = abi
+        near_contract.bytecode = bytecode
+        near_contract.source_code = source_code
+        near_contract.contract = self.contract
+        near_contract.save()
+        self.near_contract = near_contract
+        self.save()
+
 
     @blocking
     @postponable
